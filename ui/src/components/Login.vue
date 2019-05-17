bc44e63a933b8dbdef407dc8a9b5e93967c589ef
Andreas Bauer <dev@andreasbauer.org>
2019-05-17 12:22:34 +0000

    Code style guide configuration with Eslint and Editorconfig (issue #2)

--- ui/src/components/Login.vue
+++ ui/src/components/Login.vue
@@ -1,27 +1,51 @@
 <template>
   <v-toolbar-items>
-    <v-btn v-show="!isAuthenticated" flat @click="onLogin">
+    <v-btn
+      v-show="!isAuthenticated"
+      flat
+      @click="onLogin"
+    >
       Login
-      <v-icon dark right>mdi-account</v-icon>
+      <v-icon
+        dark
+        right
+      >
+        mdi-account
+      </v-icon>
     </v-btn>
 
-    <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x>
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      offset-x
+    >
       <template v-slot:activator="{ on }">
-        <v-btn v-show="isAuthenticated" v-on="on" flat to="/">
-          {{userProfile.firstName}}
-          <v-icon dark right>mdi-chevron-down</v-icon>
+        <v-btn
+          v-show="isAuthenticated"
+          flat
+          to="/"
+          v-on="on"
+        >
+          {{ userProfile.firstName }}
+          <v-icon
+            dark
+            right
+          >
+            mdi-chevron-down
+          </v-icon>
         </v-btn>
       </template>
 
       <v-list>
         <v-list-tile avatar>
           <v-list-tile-content>
-            <v-list-tile-title>{{userProfile.firstName}} {{userProfile.lastName}}</v-list-tile-title>
-            <v-list-tile-sub-title>{{userProfile.email}}</v-list-tile-sub-title>
+            <v-list-tile-title>{{ userProfile.firstName }} {{ userProfile.lastName }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ userProfile.email }}</v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
 
-        <v-divider></v-divider>
+        <v-divider />
 
         <v-list-tile @click="onEditProfile">
           <v-list-tile-title>Edit Profile</v-list-tile-title>
@@ -35,19 +59,18 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import { Action, State } from 'vuex-class';
-import { KeycloakProfile } from 'keycloak-js';
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Action, State } from 'vuex-class'
 
-const namespace = { namespace: 'auth' };
+const namespace = { namespace: 'auth' }
 
 @Component
 export default class Login extends Vue {
   private menu: boolean = false;
 
-  @State('isAuth', namespace) private isAuthenticated!: boolean;
-  @State('userProfile', namespace) private userProfile!: KeycloakProfile;
+  @State('isAuth', namespace)
+  private isAuthenticated!: boolean;
 
   @Action('login', namespace)
   private login!: () => void;
@@ -61,22 +84,22 @@ export default class Login extends Vue {
   @Action('initKeycloak', namespace)
   private initKeycloak!: () => void;
 
-  private mounted() {
-    console.log('mounted');
-    this.initKeycloak();
+  private mounted () {
+    console.log('mounted')
+    this.initKeycloak()
   }
 
-  private onLogin() {
-    this.login();
+  private onLogin () {
+    this.login()
   }
 
-  private onLogout() {
-    this.logout();
-    this.menu = false;
+  private onLogout () {
+    this.logout()
+    this.menu = false
   }
 
-  private onEditProfile() {
-    this.editProfile();
+  private onEditProfile () {
+    this.editProfile()
   }
 }
 </script>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,30 +1,30 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import App from './App.vue';
-import router from './router';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css';
-import TransformationModule from '@/transformation/module';
-import AuthModule from '@/components/auth/module';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './App.vue'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
+import TransformationModule from '@/transformation/module'
+import AuthModule from '@/components/auth/module'
 
 Vue.use(Vuetify, {
-  iconfont: 'mdi',
-});
+  iconfont: 'mdi'
+})
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 export const store = new Vuex.Store({
   modules: {
     auth: AuthModule,
-    transformation: TransformationModule,
-  },
-});
+    transformation: TransformationModule
+  }
+})
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
