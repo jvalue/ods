c317c852678d5cc8793784c99bc02910b939e056
Georg Schwarz <schwarz@group.riehle.org>
2019-06-11 05:40:17 +0000

    Adapter: handle xml files

--- adapter/src/main/java/org/jvalue/ods/adapterservice/importer/Importer.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/importer/Importer.java
@@ -3,6 +3,6 @@ package org.jvalue.ods.adapterservice.importer;
 import java.net.URI;
 
 public abstract class Importer {
-    public abstract Object fetch(URI from);
+    public abstract String fetch(URI from);
     public abstract String type();
 }
--- adapter/src/test/java/org/jvalue/ods/adapterservice/adapter/AdapterManagerTest.java
+++ adapter/src/test/java/org/jvalue/ods/adapterservice/adapter/AdapterManagerTest.java
@@ -14,4 +14,24 @@ public class AdapterManagerTest {
         assertEquals("HTTP", result.protocol());
         assertEquals("JSON", result.format());
     }
+
+    @Test
+    public void testGetHTTPXMLAdapter() {
+        AdapterConfig config = new AdapterConfig("HTTP", "XML", "location");
+        Adapter result = AdapterManager.getAdapter(config);
+        assertEquals("HTTP", result.protocol());
+        assertEquals("XML", result.format());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNotExistingProtocol() {
+        AdapterConfig config = new AdapterConfig("N/A", "XML", "location");
+        AdapterManager.getAdapter(config);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNotExistingFormat() {
+        AdapterConfig config = new AdapterConfig("HTTP", "N/A", "location");
+        AdapterManager.getAdapter(config);
+    }
 }
