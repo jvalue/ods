44e4e7dda725dd4c8dbffe53d307f2d4827a77d9
Georg Schwarz <georg.schwarz@fau.de>
2019-11-12 16:28:07 +0100

    Scheduler: move core models coming from core into core directory

--- scheduler/src/clients/adapter-client.ts
+++ scheduler/src/clients/adapter-client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 
-import PipelineConfig from '../interfaces/pipeline-config'
+import PipelineConfig from '../interfaces/core/pipeline-config'
 
 const ADAPTER_SERVICE_URL = process.env.ADAPTER_SERVICE_URL || 'http://localhost:8082'
 const ADAPTER_SERVICE_IMPORT_URL = ADAPTER_SERVICE_URL + '/dataImport'
