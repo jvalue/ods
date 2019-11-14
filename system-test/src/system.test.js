e604e46dfa60b1180049139197de72edea8dea36
Mathias Zinnen <mathias.zinnen@fau.de>
2019-11-14 12:47:06 +0100

    system-test: Create separate docker-compose service for  mock server

--- system-test/src/system.test.js
+++ system-test/src/system.test.js
@@ -6,14 +6,7 @@ const STORAGE_URL = process.env.STORAGE_API || 'http://localhost:9000/storage'
 const SCHEDULER_URL = process.env.SCHEDULER_API || 'http://localhost:9000/scheduler'
 const ADAPTER_URL = process.env.ADAPTER_API || 'http://localhost:9000/adapter'
 const TRANSFORMATION_URL = process.env.TRANSFORMATION_API || 'http://localhost:9000/transformation'
-
-const MOCK_RECEIVER_PORT = process.env.MOCK_RECEIVER_PORT || 8081
-const MOCK_RECEIVER_HOST = process.env.MOCK_RECEIVER_HOST || 'localhost'
-const MOCK_RECEIVER_URL = 'http://' + MOCK_RECEIVER_HOST + ':' + MOCK_RECEIVER_PORT
-
-const MOCK_SOURCE_PORT = process.env.MOCK_SOURCE_PORT || 8082
-const MOCK_SOURCE_HOST = process.env.MOCK_SOURCE_HOST || 'localhost'
-const MOCK_SOURCE_URL = 'http://' + MOCK_SOURCE_HOST + ':' + MOCK_SOURCE_PORT
+const MOCK_SERVER_URL = process.env.MOCK_SERVER_API || 'http://localhost:9000/system-test-mocks'
 
 const sourceData = {
   one: 1,
@@ -28,8 +21,7 @@ describe('System-Test', () => {
   beforeAll(async () => {
     console.log('Waiting for core-service with URL: ' + CORE_URL)
     console.log('Waiting for storage-service with URL: ' + STORAGE_URL)
-    console.log('Waiting for notification receiver mock with URL: ' + MOCK_RECEIVER_URL)
-    console.log('Waiting for data source mock with URL: ' + MOCK_SOURCE_URL)
+    console.log('Waiting for mock server with URL: ' + MOCK_SERVER_URL)
     await waitOn(
       { resources:
       [STORAGE_URL,
@@ -37,8 +29,8 @@ describe('System-Test', () => {
       SCHEDULER_URL,
       TRANSFORMATION_URL,
       ADAPTER_URL + '/version',
-      MOCK_SOURCE_URL,
-      MOCK_RECEIVER_URL], timeout: 10000 })
+      MOCK_SERVER_URL
+      ], timeout: 10000 })
   }, 12000)
 
   afterAll(async () => {
@@ -51,12 +43,12 @@ describe('System-Test', () => {
 
   test('Test 1: Create non-periodic pipeline without transformations', async () => {
     // Prepare datasource mock
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/data/test1')
       .send(sourceData)
 
-    const pipelineConfig = generateConfig(MOCK_SOURCE_URL+'/data/test1', false)
-    let notification = generateNotification('data.one === 1', MOCK_RECEIVER_URL+'/notifications/test1')
+    const pipelineConfig = generateConfig(MOCK_SERVER_URL+'/data/test1', false)
+    let notification = generateNotification('data.one === 1', MOCK_SERVER_URL+'/notifications/test1')
     pipelineConfig.notifications = [notification]
 
     console.log(`Test 1: Trying to create pipeline: ${JSON.stringify(pipelineConfig)}`)
@@ -67,8 +59,11 @@ describe('System-Test', () => {
       .send(pipelineConfig)
     const pipelineId = pipelineResponse.body.id
 
-    // Give the ODS time to process the pipeline
-    await sleep(8000)
+    // Wait for webhook notification
+    const webhookResponse = await checkWebhook('/test1', 1000)
+    console.log(`Test 1: Webhook response body: ${JSON.stringify(webhookResponse.body)}`)
+    expect(webhookResponse.body.location).toEqual(STORAGE_URL+'/'+pipelineId)
+    expect(webhookResponse.body.timestamp).toBeDefined()
 
     // Check if data has been stored correctly
     const storageResponse = await request(STORAGE_URL)
@@ -78,24 +73,16 @@ describe('System-Test', () => {
     console.log(`Test 1: Storage response body: ${JSON.stringify(storageResponse.body)}`)
     expect(storageResponse.body[0].data).toEqual(sourceData)
 
-    // Check if webhook was triggered
-    const webhookResponse = await request(MOCK_RECEIVER_URL)
-      .get('/notifications/test1')
-    expect(webhookResponse.status).toEqual(200)
-    expect(webhookResponse.type).toEqual('application/json')
-    console.log(`Test 1: Webhook response body: ${JSON.stringify(webhookResponse.body)}`)
-    expect(webhookResponse.body.location).toEqual(STORAGE_URL+'/'+pipelineId)
-    expect(webhookResponse.body.timestamp).toBeDefined()
   }, 10000)
 
   test('Test 2: Create periodic pipeline without transformations', async () => {
     // Prepare datasource mock
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/sequences/test2')
       .send(sourceData)
 
-    const notification = generateNotification('data.count < 2', MOCK_RECEIVER_URL+'/sequences/test2')
-    let pipelineConfig = generateConfig(MOCK_SOURCE_URL+'/sequences/test2', true, 3000)
+    const notification = generateNotification('data.count < 2', MOCK_SERVER_URL+'/notifications/test2')
+    let pipelineConfig = generateConfig(MOCK_SERVER_URL+'/sequences/test2', true, 3000)
     pipelineConfig.notifications = [notification]
 
     console.log(`Test 2: Trying to create pipeline: ${JSON.stringify(pipelineConfig)}`)
@@ -130,7 +117,7 @@ describe('System-Test', () => {
     expect(storageResponse.body[2].data.count).toEqual(2)
 
     // Check if webhooks have been triggered correctly
-    const webhookResponse = await request(MOCK_RECEIVER_URL)
+    const webhookResponse = await request(MOCK_SERVER_URL)
       .get('/sequences/test2')
     expect(webhookResponse.status).toEqual(200)
     expect(webhookResponse.type).toEqual('application/json')
@@ -144,7 +131,7 @@ describe('System-Test', () => {
 
   test('Test 3: Create non-periodic pipeline with transformations', async () => {
     // Prepare datasource mock
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/data/test3')
       .send(sourceData)
 
@@ -153,8 +140,8 @@ describe('System-Test', () => {
       two: "two",
       newField: 12
     }
-    const notification = generateNotification('data.newField === 12',MOCK_RECEIVER_URL+'/notifications/test3')
-    let pipelineConfig = generateConfig(MOCK_SOURCE_URL+'/data/test3',false)
+    const notification = generateNotification('data.newField === 12',MOCK_SERVER_URL+'/notifications/test3')
+    let pipelineConfig = generateConfig(MOCK_SERVER_URL+'/data/test3',false)
     pipelineConfig.transformations = [{"func": "data.one = data.one + 1;return data;"}, {"func": "data.newField = 12;return data;"}, {"func": "delete data.objecticus;return data;"}]
     pipelineConfig.notifications = [notification]
 
@@ -166,8 +153,11 @@ describe('System-Test', () => {
       .send(pipelineConfig)
     const pipelineId = pipelineResponse.body.id
 
-    // Give the ODS time to process the pipeline
-    await sleep(6000)
+    // Wait for webhook notification
+    const webhookResponse = await checkWebhook('/test3', 1000)
+    console.log(`Test 3: Webhook response body: ${JSON.stringify(webhookResponse.body)}`)
+    expect(webhookResponse.body.location).toEqual(STORAGE_URL+'/'+pipelineId)
+    expect(webhookResponse.body.timestamp).toBeDefined()
 
     // Check if data has been stored correctly
     const storageResponse = await request(STORAGE_URL)
@@ -177,29 +167,21 @@ describe('System-Test', () => {
     console.log(`Test 3: Storage response body: ${JSON.stringify(storageResponse.body)}`)
     expect(storageResponse.body[0].data).toEqual(expectedData)
 
-    // Check if webhook was triggered
-    const webhookResponse = await request(MOCK_RECEIVER_URL)
-      .get('/notifications/test3')
-    expect(webhookResponse.status).toEqual(200)
-    expect(webhookResponse.type).toEqual('application/json')
-    console.log(`Test 3: Webhook response body: ${JSON.stringify(webhookResponse.body)}`)
-    expect(webhookResponse.body.location).toEqual(STORAGE_URL+'/'+pipelineId)
-    expect(webhookResponse.body.timestamp).toBeDefined()
   }, 10000)
 
   test('Test 4: Update periodic pipeline', async () => {
     // Prepare datasource mock
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/data/test4')
       .send(sourceData)
     const updatedSourceData = Object.assign({}, sourceData)
     updatedSourceData.one = 2
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/data/test4_updated')
       .send(updatedSourceData)
 
     let pipelineConfig = generateConfig(
-      MOCK_SOURCE_URL+'/data/test4',true)
+      MOCK_SERVER_URL+'/data/test4',true)
     pipelineConfig.notifications = []
     pipelineConfig.trigger.interval = 8000
 
@@ -224,9 +206,9 @@ describe('System-Test', () => {
 
     // Create updated pipeline
     pipelineConfig.id = pipelineId
-    const notification = generateNotification('data.two === \"two\"', MOCK_RECEIVER_URL+'/notifications/test4')
+    const notification = generateNotification('data.two === \"two\"', MOCK_SERVER_URL+'/notifications/test4')
     pipelineConfig.notifications = [notification]
-    pipelineConfig.adapter.location = MOCK_SOURCE_URL+'/data/test4_updated'
+    pipelineConfig.adapter.location = MOCK_SERVER_URL+'/data/test4_updated'
 
     console.log(`Test 4: Pipeline ${pipelineId} should have been executed once. Update request triggered.`)
     // Update pipeline
@@ -254,7 +236,7 @@ describe('System-Test', () => {
     expect(updatedStorageResponse.body[1].data).toEqual(updatedSourceData)
 
     // Check if webhook was triggered
-    const webhookResponse = await request(MOCK_RECEIVER_URL)
+    const webhookResponse = await request(MOCK_SERVER_URL)
       .get('/notifications/test4')
     expect(webhookResponse.status).toEqual(200)
     console.log(`Test 4: Webhook response body ${JSON.stringify(webhookResponse.body)}`)
@@ -264,14 +246,14 @@ describe('System-Test', () => {
 
   test('Test 5: Create pipeline with multiple notifications', async () => {
     // Prepare datasource mock
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/data/test5')
       .send(sourceData)
 
-    let pipelineConfig = generateConfig(MOCK_SOURCE_URL+'/data/test5', false)
-    const notification1 = generateNotification('data.one === 1', MOCK_RECEIVER_URL+'/notifications/test5_1')
-    const notification2 = generateNotification('data.one === 1', MOCK_RECEIVER_URL+'/notifications/test5_2')
-    const notification3 = generateNotification('data.one < 1', MOCK_RECEIVER_URL+'/notifications/test5_3')
+    let pipelineConfig = generateConfig(MOCK_SERVER_URL+'/data/test5', false)
+    const notification1 = generateNotification('data.one === 1', MOCK_SERVER_URL+'/notifications/test5_1')
+    const notification2 = generateNotification('data.one === 1', MOCK_SERVER_URL+'/notifications/test5_2')
+    const notification3 = generateNotification('data.one < 1', MOCK_SERVER_URL+'/notifications/test5_3')
     pipelineConfig.notifications = [notification1, notification2, notification3]
 
     console.log(`Test 5: Trying to create pipeline: ${JSON.stringify(pipelineConfig)}`)
@@ -294,14 +276,14 @@ describe('System-Test', () => {
     expect(storageResponse.body[0].data).toEqual(sourceData)
 
     // Check if webhooks were triggered
-    const webhookResponse1 = await request(MOCK_RECEIVER_URL)
+    const webhookResponse1 = await request(MOCK_SERVER_URL)
       .get('/notifications/test5_1')
     expect(webhookResponse1.status).toEqual(200)
     expect(webhookResponse1.type).toEqual('application/json')
     expect(webhookResponse1.body.location).toEqual(STORAGE_URL+'/'+pipelineId)
     expect(webhookResponse1.body.timestamp).toBeDefined()
 
-    const webhookResponse2 = await request(MOCK_RECEIVER_URL)
+    const webhookResponse2 = await request(MOCK_SERVER_URL)
       .get('/notifications/test5_2')
     expect(webhookResponse2.status).toEqual(200)
     expect(webhookResponse2.type).toEqual('application/json')
@@ -309,19 +291,19 @@ describe('System-Test', () => {
     expect(webhookResponse2.body.timestamp).toBeDefined()
 
     // Check if webhooks were triggered
-    const webhookResponse3 = await request(MOCK_RECEIVER_URL)
+    const webhookResponse3 = await request(MOCK_SERVER_URL)
       .get('/notifications/test5_3')
     expect(webhookResponse3.status).toEqual(404)
   }, 10000)
 
   test('Test 6: Delete periodic pipeline', async () => {
     // Prepare datasource mock
-    await request(MOCK_SOURCE_URL)
+    await request(MOCK_SERVER_URL)
       .post('/sequences/test6')
       .send(sourceData)
 
-    const notification = generateNotification('data.two === "two"', MOCK_RECEIVER_URL+'/sequences/test6')
-    let pipelineConfig = generateConfig(MOCK_SOURCE_URL+'/sequences/test6', true, 5000)
+    const notification = generateNotification('data.two === "two"', MOCK_SERVER_URL+'/sequences/test6')
+    let pipelineConfig = generateConfig(MOCK_SERVER_URL+'/sequences/test6', true, 5000)
     pipelineConfig.notifications = [notification]
 
     console.log(`Test 6: Trying to create pipeline: ${JSON.stringify(pipelineConfig)}`)
@@ -355,7 +337,7 @@ describe('System-Test', () => {
     expect(storageResponse.body[0].data.count).toEqual(0)
 
     // Check if webhook has been triggered correctly
-    const webhookResponse = await request(MOCK_RECEIVER_URL)
+    const webhookResponse = await request(MOCK_SERVER_URL)
       .get('/sequences/test6')
     expect(webhookResponse.status).toEqual(200)
     expect(webhookResponse.type).toEqual('application/json')
@@ -399,4 +381,16 @@ function generateConfig(sourceLocation, periodic, interval = 5000) {
 
 function sleep (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function checkWebhook(uri, pollingInterval, maxRetries = 10) {
+  for(let i = 0; i < maxRetries; i++) {
+    const response = await request(MOCK_SERVER_URL)
+      .get(`/notifications/${uri}`)
+    if(response.status === 200) {
+      return response
+    } else {
+      await sleep(pollingInterval)
+    }
+  }
 }
