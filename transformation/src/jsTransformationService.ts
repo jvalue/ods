5b6b0d6b62bb5e448d651864608d17f07867f42e
Mathias Zinnen <mathias.zinnen@fau.de>
2019-11-19 15:35:27 +0100

    transformation: Fix linter error

--- transformation/src/jsTransformationService.ts
+++ transformation/src/jsTransformationService.ts
@@ -84,7 +84,8 @@ export default class JSTransformationService implements TransformationService {
 
   private async executeSlackNotification (request: NotificationRequest): Promise<void> {
     const callbackObject: SlackCallback = {
-      text: `New data available for pipeline ${request.pipelineName}(${request.pipelineId}). Fetch at ${request.dataLocation}.`
+      text: `New data available for pipeline ${request.pipelineName}(${request.pipelineId}). ` +
+        `Fetch at ${request.dataLocation}.`
     }
     await axios.post(request.url, callbackObject)
   }
