9fff19dee580549b1499f92f37ea304395e0efce
Mathias Zinnen <mathias.zinnen@fau.de>
2019-11-26 10:02:03 +0100

    transformation: Provide fcm configuration for example project

--- transformation/src/interfaces/fcmCallback.ts
+++ transformation/src/interfaces/fcmCallback.ts
@@ -1,13 +1,7 @@
 export default interface FcmCallback {
-  validate_only: boolean;
-  message: FCMMessage;
-}
-
-interface FCMMessage {
-  notification: FCMNotification;
-}
-
-interface FCMNotification {
-  title: string;
-  body: string;
+  notification: {
+    title: string;
+    body: string;
+  },
+  topic: string;
 }
