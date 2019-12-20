0312ac1c6beee886353b0410d8d5f70a07d7df43
Mathias Zinnen <mathias.zinnen@fau.de>
2019-12-20 12:28:27 +0100

    ui: Update notificationConfig model

--- ui/src/pipeline/notifications/notificationConfig.ts
+++ ui/src/pipeline/notifications/notificationConfig.ts
@@ -1,10 +1,25 @@
 export default interface NotificationConfig {
   notificationId: number;
-  notificationType: NotificationType;
   condition: string;
+  params: WebhookParams | SlackParams | FirebaseParams;
+}
+
+export interface WebhookParams {
+  type: 'WEBHOOK';
   url: string;
 }
 
-export enum NotificationType {
-  WEBHOOK = 'WEBHOOK'
+export interface SlackParams {
+  type: 'SLACK';
+  workspaceId: string;
+  channelId: string;
+  secret: string;
+}
+
+export interface FirebaseParams {
+  type: 'FCM';
+  projectId: string;
+  clientEmail: string;
+  privateKey: string;
+  topic: string;
 }
