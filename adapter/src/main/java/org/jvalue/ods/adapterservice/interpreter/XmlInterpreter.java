af9100cf18e9c7b30b51489ff33bc5a3c69d4266
Mathias Zinnen <mathias.zinnen@fau.de>
2020-01-22 12:33:04 +0100

    adapter: Implement custom deserializer for XML collections

--- adapter/src/main/java/org/jvalue/ods/adapterservice/interpreter/XmlInterpreter.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/interpreter/XmlInterpreter.java
@@ -1,7 +1,10 @@
 package org.jvalue.ods.adapterservice.interpreter;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 
 import java.io.IOException;
 import java.util.Collections;
@@ -13,7 +16,11 @@ public class XmlInterpreter extends Interpreter {
     private final List<InterpreterParameterDescription> parameters =  Collections.unmodifiableList(List.of());
     private final XmlMapper mapper = new XmlMapper();
 
-    @Override
+  public XmlInterpreter() {
+    mapper.registerModule(new SimpleModule().addDeserializer(Object.class, new UntypedXMLArrayDeserializer()));
+  }
+
+  @Override
     public String getType() {
         return "XML";
     }
@@ -26,10 +33,13 @@ public class XmlInterpreter extends Interpreter {
     @Override
     public List<InterpreterParameterDescription> getAvailableParameters() {
       return parameters;
+
     }
 
     @Override
     public JsonNode doInterpret(String data, Map<String, Object> parameters) throws IOException {
-        return mapper.readTree(data);
+      mapper.registerModule(new SimpleModule().addDeserializer(Object.class, new UntypedXMLArrayDeserializer()));
+      Object result = mapper.readValue(data, Object.class);
+      return mapper.valueToTree(result);
     }
 }
