a358cb070b0d1579a502d06055dd8201ed9cae72
Mathias Zinnen <mathias.zinnen@fau.de>
2020-02-11 13:41:22 +0100

    core: Adjust more unit tests

--- core/src/test/java/org/jvalue/ods/coreservice/model/PipelineConfigTest.java
+++ core/src/test/java/org/jvalue/ods/coreservice/model/PipelineConfigTest.java
@@ -7,6 +7,7 @@ import org.jvalue.ods.coreservice.model.adapter.AdapterConfig;
 import org.jvalue.ods.coreservice.model.adapter.AdapterFormatConfig;
 import org.jvalue.ods.coreservice.model.adapter.AdapterProtocolConfig;
 import org.jvalue.ods.coreservice.model.notification.NotificationConfig;
+import org.jvalue.ods.coreservice.model.notification.WebhookNotification;
 import org.springframework.format.datetime.DateFormatter;
 
 import java.io.File;
@@ -45,9 +46,9 @@ public class PipelineConfigTest {
     cal.setTime(result.getMetadata().getCreationTimestamp());
     assertEquals(LocalDateTime.now().getDayOfYear(), cal.get(Calendar.DAY_OF_YEAR));
     assertEquals(1, result.getNotifications().size());
-    assertNotNull(result.getNotifications().get(0).getParams().asWebhook());
+    assertNotNull(result.getNotifications().get(0));
     assertEquals("data.value1 > 10", result.getNotifications().get(0).getCondition());
-    assertEquals("http://www.webhookland.com", result.getNotifications().get(0).getParams().asWebhook().getUrl());
+    assertEquals("http://www.webhookland.com", result.getNotifications().get(0).asWebhook().getUrl());
   }
 
   @Test
@@ -58,8 +59,8 @@ public class PipelineConfigTest {
     PipelineTriggerConfig trigger = new PipelineTriggerConfig(false, new Date(), 10L);
     PipelineMetadata metadata = new PipelineMetadata("icke", "none", "Display", "description");
     List<NotificationConfig> notifications = List.of(
-        new NotificationConfig("data.value1 > 10", new NotificationConfig.WebhookParams("http://www.webhookland.com/1")),
-        new NotificationConfig("data.value1 < 0", new NotificationConfig.WebhookParams("http://www.webhookland.com/2")));
+        new WebhookNotification("data.value1 > 10", "http://www.webhookland.com/1"),
+        new WebhookNotification("data.value1 < 0", "http://www.webhookland.com/2"));
     PipelineConfig config = new PipelineConfig(adapter, transformations, trigger, metadata, notifications);
 
     JsonNode result = mapper.valueToTree(config);
@@ -76,8 +77,8 @@ public class PipelineConfigTest {
     assertEquals(2, result.get("notifications").size());
     assertEquals(3, result.get("notifications").get(0).size());
     assertEquals("data.value1 > 10", result.get("notifications").get(0).get("condition").textValue());
-    assertEquals("WEBHOOK", result.get("notifications").get(0).get("params").get("type").textValue());
-    assertEquals("http://www.webhookland.com/2", result.get("notifications").get(1).get("params").get("url").textValue());
+    assertEquals("WEBHOOK", result.get("notifications").get(0).get("type").textValue());
+    assertEquals("http://www.webhookland.com/2", result.get("notifications").get(1).get("url").textValue());
   }
 
   private AdapterConfig generateAdapterConfig(String protocol, String format, String location) {
