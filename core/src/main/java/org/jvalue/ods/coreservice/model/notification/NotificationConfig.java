68fa59a2d5f6ad44d720c89ffc9a990c65329202
Mathias Zinnen <mathias.zinnen@fau.de>
2020-02-11 13:41:44 +0100

    core: Update notification models

--- core/src/main/java/org/jvalue/ods/coreservice/model/notification/NotificationConfig.java
+++ core/src/main/java/org/jvalue/ods/coreservice/model/notification/NotificationConfig.java
@@ -1,12 +1,24 @@
 package org.jvalue.ods.coreservice.model.notification;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
-@Entity
+@MappedSuperclass
+@JsonTypeInfo(
+  use = JsonTypeInfo.Id.NAME,
+  include = JsonTypeInfo.As.PROPERTY,
+  property = "type"
+)
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = WebhookNotification.class, name = "WEBHOOK"),
+  @JsonSubTypes.Type(value = SlackNotification.class, name = "SLACK"),
+  @JsonSubTypes.Type(value = FirebaseNotification.class, name = "FCM")
+})
 public abstract class NotificationConfig {
 
   @Id
@@ -43,6 +55,33 @@ public abstract class NotificationConfig {
 
   public NotificationType getType() {
     return type;
+  }
+
+  public WebhookNotification asWebhook() {
+    if(this instanceof WebhookNotification) {
+      return (WebhookNotification) this;
+    } else {
+      throw new IllegalArgumentException("Wrong runtime class for NotificationConfig. " +
+        "Expected WebhookNotification.class, but was " + this.getClass().getCanonicalName());
+    }
+  }
+
+  public SlackNotification asSlack() {
+    if(this instanceof SlackNotification) {
+      return (SlackNotification) this;
+    } else {
+      throw new IllegalArgumentException("Wrong runtime class for NotificationConfig. " +
+        "Expected SlackNotification.class, but was " + this.getClass().getCanonicalName());
+    }
+  }
+
+  public FirebaseNotification asFirebase() {
+    if(this instanceof FirebaseNotification) {
+      return (FirebaseNotification) this;
+    } else {
+      throw new IllegalArgumentException("Wrong runtime class for NotificationConfig. " +
+        "Expected FirebaseNotification.class, but was " + this.getClass().getCanonicalName());
+    }
   }
 
   @Override
---
+++ core/src/main/java/org/jvalue/ods/coreservice/model/notification/WebhookNotification.java
@@ -0,0 +1,41 @@
+package org.jvalue.ods.coreservice.model.notification;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+public class WebhookNotification extends NotificationConfig {
+
+  @NotNull private String url;
+
+  public WebhookNotification() {
+  }
+
+  public WebhookNotification(
+    @JsonProperty(value = "condition", required = true) String condition,
+    @JsonProperty(value = "url", required = true) String url) {
+    super(condition, NotificationType.WEBHOOK);
+    this.url = url;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    WebhookNotification that = (WebhookNotification) o;
+    return Objects.equals(url, that.url);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), url);
+  }
+}
