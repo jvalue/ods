26f1827cc43b178459c97ef230c59174b02c3460
Mathias Zinnen <mathias.zinnen@fau.de>
2020-02-21 11:16:24 +0100

    adapter: Add endpoint to fetch persisted data

---
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/rest/v1/DataEndpoint.java
@@ -0,0 +1,30 @@
+package org.jvalue.ods.adapterservice.rest.v1;
+
+import org.jvalue.ods.adapterservice.model.DataBlob;
+import org.jvalue.ods.adapterservice.repository.DataBlobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/data")
+public class DataEndpoint {
+
+  private final DataBlobRepository blobRepository;
+
+  @Autowired
+  public DataEndpoint(DataBlobRepository blobRepository) {
+    this.blobRepository = blobRepository;
+  }
+
+  @GetMapping("/{id}")
+  public DataBlob getData(@PathVariable() Long id) {
+    return blobRepository.findById(id)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No data stored with id " + id));
+  }
+
+}
