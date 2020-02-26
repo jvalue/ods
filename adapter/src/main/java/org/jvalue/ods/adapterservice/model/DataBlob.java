05b416508b7ed059fce5ac35e72abf8988441b9b
Mathias Zinnen <mathias.zinnen@fau.de>
2020-02-26 13:37:08 +0100

    adapter: Refactor metadata serialization for datablob

--- adapter/src/main/java/org/jvalue/ods/adapterservice/model/DataBlob.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/model/DataBlob.java
@@ -1,13 +1,16 @@
 package org.jvalue.ods.adapterservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 public class DataBlob {
 
-  @EmbeddedId
-  private MetaData metaData;
+  @Id
+  @GeneratedValue
+  private Long id;
 
   private String data;
 
@@ -22,8 +25,12 @@ public class DataBlob {
     return data;
   }
 
+  public Long getId() {
+    return id;
+  }
+
   public MetaData getMetaData() {
-    return metaData;
+    return new MetaData(this);
   }
 
   @Override
@@ -31,12 +38,23 @@ public class DataBlob {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     DataBlob dataBlob = (DataBlob) o;
-    return Objects.equals(metaData, dataBlob.metaData) &&
+    return Objects.equals(id, dataBlob.id) &&
             Objects.equals(data, dataBlob.data);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(metaData, data);
+    return Objects.hash(id, data);
+  }
+
+  // json representation without the actual data (as response for adapter trigger)
+  public static class MetaData extends DataBlob {
+    private final Long id;
+
+
+    public MetaData(DataBlob dataBlob) {
+      this.id = dataBlob.id;
+    }
   }
 }
+
