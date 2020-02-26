98f31577c48ee1158eed9284e434dbd4676732f9
Mathias Zinnen <mathias.zinnen@fau.de>
2020-02-26 12:47:49 +0100

    adapter: Remove unused import

--- adapter/src/test/java/org/jvalue/ods/adapterservice/model/DataBlobTest.java
+++ adapter/src/test/java/org/jvalue/ods/adapterservice/model/DataBlobTest.java
@@ -1,6 +1,5 @@
 package org.jvalue.ods.adapterservice.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
