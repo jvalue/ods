d5dc136ed8ecb348319ab98b1f2d6838e91f508c
Georg Schwarz <georg.schwarz@fau.de>
2020-03-19 11:33:42 +0100

    Adapter: migrate config from core, named concept datasource and adapter as instantiation of data import for a datasource (based on this config)

---
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/datasource/DatasourceManager.java
@@ -0,0 +1,112 @@
+package org.jvalue.ods.adapterservice.datasource;
+
+import org.jvalue.ods.adapterservice.datasource.event.DatasourceEvent;
+import org.jvalue.ods.adapterservice.datasource.event.EventType;
+import org.jvalue.ods.adapterservice.datasource.model.Datasource;
+import org.jvalue.ods.adapterservice.datasource.model.DatasourceMetadata;
+import org.jvalue.ods.adapterservice.datasource.repository.DatasourceRepository;
+import org.jvalue.ods.adapterservice.datasource.repository.DatasourceEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class DatasourceManager {
+
+  private final DatasourceRepository datasourceRepository;
+  private final DatasourceEventRepository eventRepository;
+
+
+  @Autowired
+  public DatasourceManager(DatasourceRepository datasourceRepository, DatasourceEventRepository eventRepository) {
+    this.datasourceRepository = datasourceRepository;
+    this.eventRepository = eventRepository;
+  }
+
+
+  @Transactional
+  public Datasource createDatasource(Datasource config) {
+    Datasource savedConfig = datasourceRepository.save(config);
+    eventRepository.save(new DatasourceEvent(EventType.DATASOURCE_CREATE, savedConfig.getId()));
+    return savedConfig;
+  }
+
+
+  public Optional<Datasource> getDatasource(Long id) {
+    return datasourceRepository.findById(id);
+  }
+
+
+  public Iterable<Datasource> getAllDatasources() {
+    return datasourceRepository.findAll();
+  }
+
+
+  @Transactional
+  public void updateDatasource(Long id, Datasource updated) throws IllegalArgumentException {
+    Datasource old = datasourceRepository.findById(id)
+      .orElseThrow(() -> new IllegalArgumentException("Datasource with id " + id + " not found."));
+
+    datasourceRepository.save(applyUpdate(old, updated));
+    eventRepository.save(new DatasourceEvent(EventType.DATASOURCE_UPDATE, id));
+  }
+
+
+  @Transactional
+  public void deleteDatasource(Long id) {
+    datasourceRepository.deleteById(id);
+    eventRepository.save(new DatasourceEvent(EventType.DATASOURCE_DELETE, id));
+  }
+
+
+  @Transactional
+  public void deleteAllDatasources() {
+    Iterable<Datasource> allDatasourceConfigs = getAllDatasources();
+    datasourceRepository.deleteAll();
+
+    allDatasourceConfigs.forEach(
+      pl -> eventRepository.save(new DatasourceEvent(EventType.DATASOURCE_DELETE, pl.getId()))
+    );
+  }
+
+  /**
+   * Create an updated DatasourceConfig using the full representation of an update. This method ensures that id and creation time remain stable.
+   *
+   * @param updateConfig the representation of the updated config
+   * @return an updated DatasourceConfig that has the same id and creationTimestamp as the original one.
+   */
+  private Datasource applyUpdate(Datasource existing, Datasource updateConfig) {
+    DatasourceMetadata updatedMetadata = new DatasourceMetadata(
+      updateConfig.getMetadata().getAuthor(),
+      updateConfig.getMetadata().getLicense(),
+      updateConfig.getMetadata().getDisplayName(),
+      updateConfig.getMetadata().getDescription());
+    updatedMetadata.setCreationTimestamp(existing.getMetadata().getCreationTimestamp());
+
+    Datasource updated = new Datasource(
+      updateConfig.getProtocol(),
+      updateConfig.getFormat(),
+      updateConfig.getMetadata());
+    updated.setId(existing.getId());
+
+    return updated;
+  }
+
+  public Optional<DatasourceEvent> getEvent(Long id) {
+    return eventRepository.findById(id);
+  }
+
+  public Iterable<DatasourceEvent> getEventsAfter(Long id) {
+    return eventRepository.getAllByEventIdAfter(id);
+  }
+
+  public Iterable<DatasourceEvent> getEventsByDatasource(Long datasourceId) {
+    return eventRepository.getAllByDatasourceId(datasourceId);
+  }
+
+  public DatasourceEvent getLatestEvent() {
+    return eventRepository.findFirstByOrderByEventIdDesc();
+  }
+}
---
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/datasource/model/Datasource.java
@@ -0,0 +1,86 @@
+package org.jvalue.ods.adapterservice.datasource.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Embeddable
+public class Datasource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id") // referenced by embedded datasource config for format and protocol
+    private Long id;
+
+    @NotNull
+    private DatasourceProtocol protocol;
+
+    @NotNull
+    private DatasourceFormat format;
+
+    @NotNull
+    private DatasourceMetadata metadata;
+
+    //Constructor for JPA
+    private Datasource() {
+    }
+
+    @JsonCreator
+    public Datasource(
+      @JsonProperty("protocol") DatasourceProtocol protocol,
+      @JsonProperty("format") DatasourceFormat format,
+      @JsonProperty("metadata") DatasourceMetadata metadata) {
+        this.protocol = protocol;
+        this.format = format;
+        this.metadata = metadata;
+    }
+
+    public Long getId() {
+      return id;
+    }
+
+    public void setId(Long id) {
+      this.id = id;
+    }
+
+    public DatasourceProtocol getProtocol() {
+          return protocol;
+      }
+
+    public DatasourceFormat getFormat() {
+        return format;
+    }
+
+    public DatasourceMetadata getMetadata() {
+      return metadata;
+    }
+
+    @Override
+    public String toString() {
+      return "Datasource{" +
+        "id=" + id +
+        ", protocol=" + protocol +
+        ", format=" + format +
+        ", metadata=" + metadata +
+        '}';
+    }
+
+  @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Datasource)) return false;
+      Datasource that = (Datasource) o;
+      return Objects.equals(id, that.id) &&
+        Objects.equals(protocol, that.protocol) &&
+        Objects.equals(format, that.format) &&
+        Objects.equals(metadata, that.metadata);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(id, protocol, format, metadata);
+    }
+}
