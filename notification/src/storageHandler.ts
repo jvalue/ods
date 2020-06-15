4a7b783ad302a51b947fcaa4532750e89e223d6a
Georg Schwarz <dev@georg-schwarz.com>
2020-06-15 18:11:33 +0200

    Remove unnecessary logs

--- notification/src/storageHandler.ts
+++ notification/src/storageHandler.ts
@@ -1,16 +1,31 @@
 import { SlackConfig, WebHookConfig, FirebaseConfig } from './interfaces/notificationConfig';
 import { Connection, ConnectionOptions, createConnection, getConnection } from 'typeorm';
 import { NotificationSummary} from './interfaces/notificationSummary';
+import { NotificationRepository } from './interfaces/notificationRepository';
 
 /**=============================================================================================================
  * This class handles Requests to the Nofification Database
  * in order to store and get Notification Configurations.
  *==============================================================================================================*/
-export class StorageHandler {
+export class StorageHandler implements NotificationRepository {
 
-    constructor() {
-
+    private connectionOptions: ConnectionOptions = {
+      type: "postgres",
+      host: process.env.PGHOST,
+      port: +process.env.PGPORT!,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGUSER,
+      synchronize: true,
+      // logging: true,
+      entities: [
+          WebHookConfig,
+          SlackConfig,
+          FirebaseConfig
+      ]
     }
+
+    constructor() { }
     /**===========================================================================================
      * Initializes a Database Connection to the notification-db service (postgres)
      * by using the Environment variables:
@@ -24,55 +39,26 @@ export class StorageHandler {
      *
      * @returns     a Promise, containing either a Connection on success or null on failure
      *===========================================================================================*/
-    public async initConnection(retries: number, backoff: number): Promise<Connection | null> {
+    public async initConnection(retries: number, backoff: number): Promise<void> {
         var dbCon: null | Connection = null
-        var established: boolean = false
-
-        const options: ConnectionOptions = {
-            type: "postgres",
-            host: process.env.PGHOST,
-            port: +process.env.PGPORT!,
-            username: process.env.PGUSER,
-            password: process.env.PGPASSWORD,
-            database: process.env.PGUSER,
-            synchronize: true,
-            // logging: true,
-            entities: [
-                WebHookConfig,
-                SlackConfig,
-                FirebaseConfig
-            ]
-        }
+        var connected: boolean = false
 
         // try to establish connection
         for (let i = 0; i < retries; i++) {
-            dbCon = await createConnection(options).catch(() => { return null })
-
+            dbCon = await createConnection(this.connectionOptions).catch(() => { return null })
             if (!dbCon) {
-                console.warn(`DB Connection could not be initialized. Retrying in ${backoff} seconds`)
+                console.info(`DB Connection could not be initialized. Retrying in ${backoff} seconds`)
                 await this.backOff(backoff);
             } else {
-                established = true
+                connected = true
                 break;
             }
         }
 
-        if (established) {
-            console.log('Connection established')
-        } else {
-            console.error('Connection could not be established.')
+        if (!connected) {
+            return Promise.reject("Connection to databse could not be established.")
         }
-
-        return dbCon
-    }
-
-    /**====================================================================
-     * Waits for a specific time period
-     *
-     * @param backOff   Period to wait in seconds
-     *====================================================================*/
-    private backOff(backOff: number): Promise<void> {
-        return new Promise(resolve => setTimeout(resolve, backOff * 1000));
+        return Promise.resolve()
     }
 
     /**
@@ -93,24 +79,8 @@ export class StorageHandler {
         notificationSummary.firebase = firebaseConfig
 
         return notificationSummary
-        // // wait for the Configs to be received from DB
-        // await Promise.all([webHookConfigs, slackConfigs, firebaseConfig]).then(configs => {
-
-        //     if (!configs || !configs[0] || !configs[1] || !configs[2]) {
-        //         console.error('Could not get Configs from database.')
-        //         return null
-        //     }
-
-        //     notificationSummary.webhook = configs[0] as WebHookConfig[]
-        //     notificationSummary.slack = configs[1] as SlackConfig[]
-        //     notificationSummary.firebase = configs[2] as FirebaseConfig[]
-
-
-        //     // return notificationSummary
-        //     return configs
-        // })
-
     }
+
     /**===========================================================================================
      * Gets all Slack Config from the database for a specific pipeline id
      *
@@ -120,12 +90,10 @@ export class StorageHandler {
         console.log(`Getting Slack Configs with pipelineId ${pipelineId} from Database`)
         var slackConfigList: SlackConfig[] = []
 
-        // Get Configs from Database
         try {
             const repository = getConnection().getRepository(SlackConfig)
             slackConfigList = await repository.find({ pipelineId: pipelineId })
         } catch (error) {
-            console.log(error)
             Promise.reject(error)
         }
         console.log(`Sucessfully got ${slackConfigList.length} Slack config(s) from Database`)
@@ -141,12 +109,10 @@ export class StorageHandler {
         console.log(`Getting WebHook Configs with pipelineId ${pipelineId} from Database`)
         var webHookConfigs: WebHookConfig[] = []
 
-        // Get Configs from Database
         try {
             const repository = getConnection().getRepository(WebHookConfig)
             webHookConfigs = await repository.find({ pipelineId: pipelineId })
         } catch (error) {
-            console.log(error)
             Promise.reject(error)
         }
         console.log(`Sucessfully got ${webHookConfigs.length} WebhookConfigs from Database`)
@@ -160,15 +126,12 @@ export class StorageHandler {
      *============================================================================================*/
     public async getFirebaseConfigs(pipelineId: number): Promise<FirebaseConfig[]> {
         console.log(`Getting Firebase Configs with pipelineId ${pipelineId} from Database`)
-
         var firebaseConfigs: FirebaseConfig[] = []
 
-        // Get Configs from Database
         try {
             const repository = getConnection().getRepository(FirebaseConfig)
             firebaseConfigs = await repository.find({ pipelineId: pipelineId })
         } catch (error) {
-            console.log(error)
             Promise.reject(error)
         }
         console.log(`Sucessfully got ${firebaseConfigs.length} Firebase configs from Database`)
@@ -179,21 +142,12 @@ export class StorageHandler {
      * TODO: Document
      *
      *===============================================================*/
-    public saveWebhookConfig(webhookConfig: WebHookConfig): boolean{
-
-        // Init Repository for WebHook Config
-        console.debug("Init Repository")
+    public saveWebhookConfig(webhookConfig: WebHookConfig): boolean {
         const postRepository = getConnection().getRepository(WebHookConfig)
-
-        // create object from Body of the Request (=WebHookConfig)
-        console.debug("Init Webhook config")
         webhookConfig = postRepository.create(webhookConfig)
 
         // persist the Config
-        console.debug("Save WebHookConfig to Repository")
         postRepository.save(webhookConfig);
-        console.log("Webhook config persisted")
-
         return true
     }
 
@@ -202,19 +156,11 @@ export class StorageHandler {
      *
      *===============================================================*/
     public saveSlackConfig(slackConfig: SlackConfig): boolean {
-        // Init Repository for Slack Config
-        console.debug("Init Repository")
         const postRepository = getConnection().getRepository(SlackConfig)
-
-        // create object from Body of the Request (=SlackConfig)
-        console.debug("Init SlackConfig")
         slackConfig = postRepository.create(slackConfig)
 
         // persist the Config
-        console.debug("Save SlackConfig to Repository")
         postRepository.save(slackConfig);
-        console.log("Slack config persisted")
-
         return true
     }
 
@@ -223,21 +169,20 @@ export class StorageHandler {
      *
      *===============================================================*/
     public saveFirebaseConfig(firebaseConfig: FirebaseConfig): boolean {
-        // Init Repository for Slack Config
-        console.debug("Init Repository")
         const postRepository = getConnection().getRepository(FirebaseConfig)
-
-        // create object from Body of the Request (=FirebaseConfig)
-        console.debug("Init FirebaseConfig")
         firebaseConfig = postRepository.create(firebaseConfig)
 
-
-        // persist the Config
-        console.debug("Save FireBase config to Repository")
         postRepository.save(firebaseConfig);
-        console.log("FireBase config persisted")
-
         return true
     }
+
+    /**====================================================================
+     * Waits for a specific time period
+     *
+     * @param backOff   Period to wait in seconds
+     *====================================================================*/
+    private backOff(backOff: number): Promise<void> {
+      return new Promise(resolve => setTimeout(resolve, backOff * 1000));
+  }
 
 }
