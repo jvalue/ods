ab338936e59c6f2f9ff4e06ebda0bfbab1d4bb52
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-06-19 13:35:44 +0200

    Adapter: Define queue name in rabbit configuration

--- adapter/src/main/java/org/jvalue/ods/adapterservice/config/RabbitConfiguration.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/config/RabbitConfiguration.java
@@ -1,14 +1,12 @@
 package org.jvalue.ods.adapterservice.config;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfiguration {
-    @Value("${queues.dataImportQueue}")
-    public static String DATA_IMPORT_QUEUE;
+    public static final String DATA_IMPORT_QUEUE = "dataImportQueue";
 
     @Bean
     public Queue dataImportQueue() {
