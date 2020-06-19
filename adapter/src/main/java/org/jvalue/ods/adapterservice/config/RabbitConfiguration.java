c78861a5342af0d61bb4d21725eacfbc40023405
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-06-19 13:24:44 +0200

    Adapter: Push imported data to rabbitmq

--- adapter/src/main/java/org/jvalue/ods/adapterservice/config/RabbitConfiguration.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/config/RabbitConfiguration.java
@@ -1,13 +1,17 @@
 package org.jvalue.ods.adapterservice.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfiguration {
+    @Value("${queues.dataImportQueue}")
+    public static String DATA_IMPORT_QUEUE;
+
     @Bean
     public Queue dataImportQueue() {
-        return new Queue("dataImportQueue");
+        return new Queue(DATA_IMPORT_QUEUE);
     }
 }
