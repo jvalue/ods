461ff3dd3ed42868fafa241f0f49eb0abd43d736
Jens Wächtler <jens.f.waechtler@gmail.com>
2020-06-25 14:58:52 +0200

    Add trigger endpoint for datasources

--- adapter/src/main/java/org/jvalue/ods/adapterservice/datasource/rest/v1/DatasourceEndpoint.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/datasource/rest/v1/DatasourceEndpoint.java
@@ -1,10 +1,15 @@
 package org.jvalue.ods.adapterservice.datasource.rest.v1;
 
+import org.jvalue.ods.adapterservice.adapter.Adapter;
+import org.jvalue.ods.adapterservice.adapter.AdapterFactory;
+import org.jvalue.ods.adapterservice.adapter.model.DataBlob;
 import org.jvalue.ods.adapterservice.datasource.DatasourceManager;
 import org.jvalue.ods.adapterservice.datasource.model.Datasource;
+import org.jvalue.ods.adapterservice.datasource.model.DatasourceParameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,10 +23,12 @@ import java.net.URI;
 public class DatasourceEndpoint {
 
     private final DatasourceManager datasourceManager;
+    private final AdapterFactory adapterFactory;
 
     @Autowired
-    public DatasourceEndpoint(DatasourceManager datasourceManager) {
+    public DatasourceEndpoint(DatasourceManager datasourceManager, AdapterFactory adapterFactory) {
         this.datasourceManager = datasourceManager;
+      this.adapterFactory = adapterFactory;
     }
 
     @GetMapping
@@ -71,5 +78,38 @@ public class DatasourceEndpoint {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAllDatasources() {
         datasourceManager.deleteAllDatasources();
+    }
+
+    @PostMapping("/{id}/trigger")
+    public DataBlob.MetaData getData(@PathVariable() Long id, @Valid @RequestBody DatasourceParameters datasourceParameters) {
+      Datasource datasource = datasourceManager.getDatasource(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No datasource found with id " + id));
+      try {
+        String url = (String) datasource.getProtocol().getParameters().get("location");
+      } catch (Exception e) {
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "No baseurl for this datasource");
+      }
+      if (datasourceParameters.parameters != null) {
+        datasourceParameters.parameters.forEach((k,v) -> {
+          System.out.println(k); // fill parameters
+        });
+      }
+      try {
+        Adapter adapter = adapterFactory.getAdapter(datasource.toAdapterConfig());
+        return adapter.executeJob(datasource.toAdapterConfig());
+      } catch (Exception e) {
+        if(e instanceof HttpMessageNotReadableException) {
+          System.err.println("Data Import request failed. Malformed Request: " + e.getMessage());
+          throw e;
+        }
+        String location = datasource.toAdapterConfig().protocolConfig.parameters.get("location").toString();
+        if(location != null) {
+          System.err.println("Importing data from " + location + " failed.\n" +
+            "Reason: " + e.getClass().getName() + ": " + e.getMessage());
+        } else {
+          System.err.println("Data Import failed. Reason: " + e.getClass() + ": " +e.getMessage());
+        }
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
     }
 }
