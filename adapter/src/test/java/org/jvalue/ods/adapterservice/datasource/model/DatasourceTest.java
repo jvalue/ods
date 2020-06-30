21826c9e9004e7a72ecc40dfa6e15343175c3413
Jens Wächtler <jens.f.waechtler@gmail.com>
2020-06-30 12:00:32 +0200

    Remove unused import

--- adapter/src/test/java/org/jvalue/ods/adapterservice/datasource/model/DatasourceTest.java
+++ adapter/src/test/java/org/jvalue/ods/adapterservice/datasource/model/DatasourceTest.java
@@ -1,6 +1,5 @@
 package org.jvalue.ods.adapterservice.datasource.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
