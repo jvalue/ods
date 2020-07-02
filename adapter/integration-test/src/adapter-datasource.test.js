2f11bfd09c2f99e91a95b32fd3e768486a1d4cec
Jens Wächtler <jens.f.waechtler@gmail.com>
2020-07-02 17:37:55 +0200

    Add check if mock server and rabbitmq is online

--- adapter/integration-test/src/adapter-datasource.test.js
+++ adapter/integration-test/src/adapter-datasource.test.js
@@ -5,6 +5,7 @@ const URL = process.env.ADAPTER_API || 'http://localhost:9000/api/adapter'
 const MOCK_SERVER_PORT = process.env.MOCK_SERVER_PORT || 8081
 const MOCK_SERVER_HOST = process.env.MOCK_SERVER_HOST || 'localhost'
 const MOCK_SERVER_URL = 'http://' + MOCK_SERVER_HOST + ':' + MOCK_SERVER_PORT
+const RABBIT_URL = `http://${process.env.RABBIT_HOST}:15672`
 
 describe('Adapter Configuration', () => {
   console.log('Adapter-Service URL= ' + URL)
@@ -13,8 +14,12 @@ describe('Adapter Configuration', () => {
     try {
       const pingUrl = URL + '/version'
       console.log('Waiting for service with URL: ' + pingUrl)
-      await waitOn({ resources: [pingUrl], timeout: 50000 })
+      console.log('Waiting for service with URL: ' + MOCK_SERVER_URL)
+      console.log('Waiting for service with URL: ' + RABBIT_URL)
+      await waitOn({ resources: [pingUrl, MOCK_SERVER_URL, RABBIT_URL], timeout: 50000 })
       console.log('[online] Service with URL:  ' + pingUrl)
+      console.log('[online] Service with URL:  ' + MOCK_SERVER_URL)
+      console.log('[online] Service with URL:  ' + RABBIT_URL)
     } catch (err) {
       process.exit(1)
     }
