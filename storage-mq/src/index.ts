24beb11e3caa5b6f11f2555ce8e0914021930b2d
Georg Schwarz <dev@georg-schwarz.com>
2020-07-13 17:25:29 +0200

    Storage-mq: first implementation of pipeline execution event consumption and handling

--- storage-mq/src/index.ts
+++ storage-mq/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import { StorageHandler } from './handlers/storageHandler';
+import "reflect-metadata";
+import { AmqpHandler } from './handlers/amqpHandler';
+import { DataEndPoint } from './dataEndpoint';
+const port = 8080
+
+
+const storageHandler = new StorageHandler()
+const amqpHandler = new AmqpHandler(storageHandler)
+
+const notificationEndpoint = new DataEndPoint(storageHandler, amqpHandler, port)
+
+notificationEndpoint.listen()
