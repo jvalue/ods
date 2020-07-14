5ed1ecf102b3fa75dc867e2544c2244965255b97
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-07-14 20:31:37 +0200

    Use static amqp client instead of class

--- scheduler/src/clients/amqp-client.ts
+++ scheduler/src/clients/amqp-client.ts
@@ -3,63 +3,62 @@ import amqp from 'amqplib'
 const AMQP_HOST = process.env.AMQP_HOST
 const AMQP_USR = process.env.AMQP_USR
 const AMQP_PWD = process.env.AMQP_PWD
-const AMQP_EXCHANGE = process.env.AMQP_EXCHANGE || 'ods'
+const AMQP_EXCHANGE = process.env.AMQP_EXCHANGE || 'ods_global'
 const AMQP_KEY_SCHED = process.env.AMQP_KEY_SCHED || 'scheduler'
 const AMQP_URL = `amqp://${AMQP_USR}:${AMQP_PWD}@${AMQP_HOST}`
 
-export class AmqpClient {
-  private channel: amqp.Channel | undefined
-  private initialized = false
 
-  public async init (): Promise<void> {
-    if(!this.initialized) {
-      try {
-        const connection = await this.connect()
-        this.channel = await this.initChannel(connection)
-        this.initialized = true
-      } catch (error) {
-        console.error(`Error initializing the AMQP Client: ${error}`)
-      }
-    } else {
-      console.log('AmqpClient initialization aborted: Already initialized.')
-      return
-    }
-  }
+let channel: amqp.Channel
+let initialized = false
 
-  private async connect (): Promise<amqp.Connection> {
+export async function init(): Promise<void> {
+  if (!initialized) {
     try {
-      const connection = await amqp.connect(AMQP_URL)
-      console.log(`Connection to amqp host at ${AMQP_HOST} successful`)
-      return connection
+      const connection = await connect()
+      channel = await initChannel(connection)
+      initialized = true
     } catch (error) {
-      console.error(`Error connecting to amqp host at ${AMQP_HOST}: ${error}`)
-      throw error
+      console.error(`Error initializing the AMQP Client: ${error}`)
     }
+  } else {
+    console.log('AmqpClient initialization aborted: Already initialized.')
+    return
   }
+}
 
-  private async initChannel (connection: amqp.Connection): Promise<amqp.Channel> {
+async function connect(): Promise<amqp.Connection> {
+  try {
+    const connection = await amqp.connect(AMQP_URL)
+    console.log(`Connection to amqp host at ${AMQP_HOST} successful`)
+    return connection
+  } catch (error) {
+    console.error(`Error connecting to amqp host at ${AMQP_HOST}: ${error}`)
+    throw error
+  }
+}
+
+async function initChannel(connection: amqp.Connection): Promise<amqp.Channel> {
+  try {
+    const channel = await connection.createChannel()
+    await channel.assertExchange(AMQP_EXCHANGE, 'topic', {durable: true})
+    console.log(`Exchange ${AMQP_EXCHANGE} successfully initialized.`)
+    return channel
+  } catch (error) {
+    console.error(`Error creating exchange ${AMQP_EXCHANGE}: ${error}`)
+    throw error
+  }
+}
+
+export function publish(content: NotificationTriggerEvent): boolean {
+  if (!initialized || !channel) {
+    console.error('Publish not possible, AMQP client not initialized.')
+    return false
+  } else {
     try {
-      const channel = await connection.createChannel()
-      await channel.assertExchange(AMQP_EXCHANGE, 'topic', { durable: true })
-      console.log(`Exchange ${AMQP_EXCHANGE} successfully initialized.`)
-      return channel
+      return channel.publish(AMQP_EXCHANGE, AMQP_KEY_SCHED, Buffer.from(content))
     } catch (error) {
-      console.error(`Error creating exchange ${AMQP_EXCHANGE}: ${error}`)
-      throw error
-    }
-  }
-
-  public publish (content: NotificationTriggerEvent): boolean {
-    if (!this.channel) {
-      console.error('Publish not possible, AMQP client not initialized.')
+      console.error(`Error publishing to exchange ${AMQP_EXCHANGE} under key ${AMQP_KEY_SCHED}: ${error}`)
       return false
-    } else {
-      try {
-        return this.channel.publish(AMQP_EXCHANGE, AMQP_KEY_SCHED, Buffer.from(content))
-      } catch (error) {
-        console.error(`Error publishing to exchange ${AMQP_EXCHANGE} under key ${AMQP_KEY_SCHED}: ${error}`)
-        return false
-      }
     }
   }
 }
