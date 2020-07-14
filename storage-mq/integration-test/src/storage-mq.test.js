54b0185857f6fcfc22c407c9fdfdd3d73995bf75
Georg Schwarz <schwarz@group.riehle.org>
2020-07-14 10:35:58 +0200

    Storage-mq: wait for storage-mq service running in integration tests

--- storage-mq/integration-test/src/storage-mq.test.js
+++ storage-mq/integration-test/src/storage-mq.test.js
@@ -1,9 +1,15 @@
+const waitOn = require('wait-on')
 
+const URL = process.env.STORAGEMQ_API
+const AMQP_URL = process.env.AMQP_URL
 
 describe('Storage-MQ', () => {
+  console.log('Storage-MQ URL= ' + URL)
 
   beforeAll(async () => {
     console.log("Waiting on all dependent services before starting to test")
+    const pingUrl = URL + '/'
+    await waitOn({ resources: [pingUrl], timeout: 50000 , log: true })
   }, 60000)
 
   test('GET /version', async () => {
