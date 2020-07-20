31f25908f8fcc6c2b061cf62f73a32a0262e15f9
Jens Wächtler <jens.f.waechtler@gmail.com>
2020-07-20 09:57:40 +0200

    Add replacement of default values for a datasource

--- adapter/src/main/java/org/jvalue/ods/adapterservice/datasource/model/Datasource.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/datasource/model/Datasource.java
@@ -11,6 +11,10 @@ import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Entity
 public class Datasource {
@@ -96,7 +100,10 @@ public class Datasource {
         return this.getProtocol();
       }
       String url = (String) this.getProtocol().getParameters().get("location");
-      for (Map.Entry<String, String> parameter : runtimeParameters.parameters.entrySet()) {
+      Map<String, String> defaultParameters = (Map<String, String>) this.getProtocol().getParameters().getOrDefault("defaultParameters", new HashMap<String, String>());
+      Map<String, String> triggerParameters = runtimeParameters.parameters;
+      Map<String, String> replacementParameters = Stream.of(defaultParameters, triggerParameters).flatMap(map -> map.entrySet().stream()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (v1, v2) -> v2));
+      for (Map.Entry<String, String> parameter : replacementParameters.entrySet()) {
         url = url.replace("{" + parameter.getKey() + "}", parameter.getValue());
       }
       HashMap<String, Object> parameters = new HashMap<>(this.getProtocol().getParameters());
