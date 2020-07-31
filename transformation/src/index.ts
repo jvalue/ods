e1f7352cab9ad3dc9d59b30274ed44877fb9706b
Georg Schwarz <schwarz@group.riehle.org>
2020-07-31 09:31:08 +0200

    Transformation: refactor express server creation to index and remove keycloak auth

--- transformation/src/index.ts
+++ transformation/src/index.ts
@@ -1,17 +1,48 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 import { PipelineExecutionEndpoint } from '@/api/rest/pipelineExecutionEndpoint'
 import VM2SandboxExecutor from '@/pipeline-execution/sandbox/vm2SandboxExecutor'
 import PipelineExecutor from '@/pipeline-execution/pipelineExecutor'
-
 const port = 8080
 
-// authEnabled defaults to false
-const authEnabled: boolean = process.env.AUTH_ENABLED === 'true'
-if (authEnabled === false) {
-  console.warn('WARNING: Authentication is disabled! Make sure this option turned off in production!\n')
-}
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const sandboxExecutor = new VM2SandboxExecutor()
-const transformationService = new PipelineExecutor(sandboxExecutor)
-const transformationEndpoint = new PipelineExecutionEndpoint(transformationService, port, authEnabled)
+const pipelineExecutor = new PipelineExecutor(sandboxExecutor)
 
-transformationEndpoint.listen()
+// global promise-rejected handler
+process.on('unhandledRejection', function(reason, p){
+  console.debug("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+});
+
+
+const server = app.listen(port, async () => {
+  console.log('Listening on port ' + port)
+
+  const pipelineExecutionEndpoint = new PipelineExecutionEndpoint(pipelineExecutor, app)
+
+  app.get("/", (req: express.Request, res: express.Response): void => {
+    res.status(200)
+        .send('I am alive!')
+  })
+
+  app.get("/version", (req: express.Request, res: express.Response): void => {
+    res.header('Content-Type', 'text/plain')
+    res.status(200)
+        .send(pipelineExecutor.getVersion())
+    res.end()
+  })
+})
+
+process.on('SIGTERM', async () => {
+  console.info('Tramsformation-Service: SIGTERM signal received.')
+  await server.close()
+})
+
+
