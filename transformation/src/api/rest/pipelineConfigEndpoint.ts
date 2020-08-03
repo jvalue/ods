a83529562eb02d160cb00b7a24eec0dbebb0fbd4
Georg Schwarz <schwarz@group.riehle.org>
2020-08-03 11:19:14 +0200

    Transformation: add endpoints, manger delegation, and initialization of config handling

--- transformation/src/api/rest/pipelineConfigEndpoint.ts
+++ transformation/src/api/rest/pipelineConfigEndpoint.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 import PipelineConfigTriggerRequest from './pipelineConfigTriggerRequest'
 import { PipelineConfigManager } from '../../pipeline-config/pipelineConfigManager'
+import PipelineConfig from '@/pipeline-config/model/pipelineConfig'
 
 
 export class PipelineConfigEndpoint {
@@ -12,6 +13,12 @@ export class PipelineConfigEndpoint {
     this.pipelineConfigManager = pipelineConfigManager
 
     app.post('/config/:id/trigger', this.triggerConfigExecution)
+    app.get('/configs', this.getAll)
+    app.get('/configs/:id', this.getOne)
+    app.post('/configs', this.create)
+    app.put('/configs/:id', this.update)
+    app.delete('/configs/:id', this.delete)
+    app.delete('/configs', this.deleteAll)
   }
 
   // The following methods need arrow syntax because of javascript 'this' shenanigans
@@ -41,7 +48,89 @@ export class PipelineConfigEndpoint {
     const answer = `Triggered pipeline ${triggerRequest.pipelineId}. Executing asynchronously...`
     res.setHeader('Content-Type', 'text/plain')
     res.writeHead(200)
-    res.write(answer)
+    res.write(JSON.stringify(answer))
+    res.end()
+  }
+
+  delete = async (req: express.Request, res: express.Response): Promise<void> => {
+    const configId = req.params.id
+    if(!configId) {
+      res.writeHead(400)
+      res.write("Path parameter id is missing or is incorrect")
+      res.end()
+      return
+    }
+    const config = await this.pipelineConfigManager.delete(+configId)
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(204)
+    res.end()
+  }
+
+
+  deleteAll = async (req: express.Request, res: express.Response): Promise<void> => {
+    const config = await this.pipelineConfigManager.deleteAll()
+    res.writeHead(204)
+    res.end()
+  }
+
+  update = async (req: express.Request, res: express.Response): Promise<void> => {
+    const configId = req.params.id
+    if(!configId) {
+      res.writeHead(400)
+      res.write("Path parameter id is missing or is incorrect")
+      res.end()
+      return
+    }
+    const config = req.body as PipelineConfig
+    await this.pipelineConfigManager.update(+configId, config)
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(204)
+    res.end()
+  }
+
+  create = async (req: express.Request, res: express.Response): Promise<void> => {
+    const config = req.body as PipelineConfig
+    const savedConfig = await this.pipelineConfigManager.create(config)
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader("location", `/configs/${savedConfig.id}`)
+    res.writeHead(201)
+    res.write(JSON.stringify(savedConfig))
+    res.end()
+  }
+
+  getByDatasourceId = async (datasourceId: number, req: express.Request, res: express.Response): Promise<void> => {
+    const configs = await this.pipelineConfigManager.getByDatasourceId(datasourceId)
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200)
+    res.write(JSON.stringify(configs))
+    res.end()
+  }
+
+  getOne = async (req: express.Request, res: express.Response): Promise<void> => {
+    const configId = req.params.id
+    if(!configId) {
+      res.writeHead(400)
+      res.write("Path parameter id is missing or is incorrect")
+      res.end()
+      return
+    }
+    const config = await this.pipelineConfigManager.get(+configId)
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200)
+    res.write(JSON.stringify(config))
+    res.end()
+  }
+
+  getAll = async (req: express.Request, res: express.Response): Promise<void> => {
+    const datasourceId = req.query.datasourceId
+    if(datasourceId) {
+      return this.getByDatasourceId(+datasourceId, req, res)
+    }
+
+    const configs = await this.pipelineConfigManager.getAll()
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200)
+    res.write(JSON.stringify(configs))
     res.end()
   }
 }
