f056171a216ced0d1913ae2bd7e88cc023041f5e
Georg Schwarz <schwarz@group.riehle.org>
2020-08-05 13:39:10 +0200

    Transformation: run linter and fix all issues except environment variables

--- transformation/src/pipeline-config/publisher/amqpExecutionResultPublisher.ts
+++ transformation/src/pipeline-config/publisher/amqpExecutionResultPublisher.ts
@@ -1,27 +1,23 @@
-import * as AMQP from "amqplib"
-
-import { ExecutionResultPublisher } from "./executionResultPublisher";
-import AmqpPublisher from "./amqpPublisher";
+import AmqpPublisher from './amqpPublisher'
+import { ExecutionResultPublisher } from './executionResultPublisher'
 
 const AMQP_URL = process.env.AMQP_URL!
 const AMQP_EXCHANGE = process.env.AMQP_PIPELINE_EXECUTION_EXCHANGE!
 const AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC = process.env.AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC!
 const AMQP_PIPELINE_EXECUTION_ERROR_TOPIC = process.env.AMQP_PIPELINE_EXECUTION_ERROR_TOPIC!
 
-
 export default class AmqpExecutionResultPublisher implements ExecutionResultPublisher {
-
   private publisher: AmqpPublisher
 
-  constructor() {
+  constructor () {
     this.publisher = new AmqpPublisher()
   }
 
-  init(retries: number, msBackoff: number) {
+  init (retries: number, msBackoff: number): Promise<void> {
     return this.publisher.init(AMQP_URL, AMQP_EXCHANGE, retries, msBackoff)
   }
 
-  publishError(pipelineId: number, pipelineName: string, errorMsg: string): boolean {
+  publishError (pipelineId: number, pipelineName: string, errorMsg: string): boolean {
     const content = {
       pipelineId: pipelineId,
       pipelineName: pipelineName,
@@ -30,7 +26,7 @@ export default class AmqpExecutionResultPublisher implements ExecutionResultPubl
     return this.publisher.publish(AMQP_EXCHANGE, AMQP_PIPELINE_EXECUTION_ERROR_TOPIC, content)
   }
 
-  publishSuccess(pipelineId: number, pipelineName: string, result: object): boolean {
+  publishSuccess (pipelineId: number, pipelineName: string, result: object): boolean {
     const content = {
       pipelineId: pipelineId,
       pipelineName: pipelineName,
@@ -38,5 +34,4 @@ export default class AmqpExecutionResultPublisher implements ExecutionResultPubl
     }
     return this.publisher.publish(AMQP_EXCHANGE, AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC, content)
   }
-
 }
