7f299f3222ffa7947c98dc840d52b7e0d707d18e
jsone-studios <jsone-studios@users.noreply.github.com>
2020-08-07 15:20:56 +0200

    Remove auth service from ui

--- ui/src/pipeline/edit/transformation/transformationRest.ts
+++ ui/src/pipeline/edit/transformation/transformationRest.ts
@@ -1,13 +1,17 @@
-import { AxiosResponse } from 'axios'
-import { createAxios } from '@/keycloak'
+import axios, { AxiosResponse } from 'axios'
 
 import { TransformationRequest, JobResult } from './transformation'
 
 const TRANSFORMATION_SERVICE_URL = process.env.VUE_APP_TRANSFORMATION_SERVICE_URL as string
 
-export async function transformData (request: TransformationRequest): Promise<JobResult> {
-  const http = await createAxios(TRANSFORMATION_SERVICE_URL)
+const http = axios.create({
+  baseURL: TRANSFORMATION_SERVICE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
 
+export async function transformData (request: TransformationRequest): Promise<JobResult> {
   const response = await http.post('/job', request, {
     validateStatus: status => (status >= 200 && status <= 400)
   }) as AxiosResponse<JobResult>
