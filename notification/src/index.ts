e9b93826c113dcc93a6ae84639e7a08aee67528d
jsone-studios <jsone-studios@users.noreply.github.com>
2020-08-07 13:08:10 +0200

    Remove auth service from services

--- notification/src/index.ts
+++ notification/src/index.ts
@@ -2,23 +2,17 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 
-import "reflect-metadata"; // once required for orm
+import 'reflect-metadata' // once required for orm
 
 import NotificationExecutor from './notification-execution/notificationExecutor'
 import VM2SandboxExecutor from './notification-execution/condition-evaluation/vm2SandboxExecutor'
-import { NotificationConfigEndpoint } from './api/rest/notificationConfigEndpoint';
-import { NotificationExecutionEndpoint } from './api/rest/notificationExecutionEndpoint';
-import { StorageHandler } from './notification-config/storageHandler';
-import { AmqpHandler } from './api/amqp/amqpHandler';
-import { TriggerEventHandler } from './api/triggerEventHandler';
+import { NotificationConfigEndpoint } from './api/rest/notificationConfigEndpoint'
+import { NotificationExecutionEndpoint } from './api/rest/notificationExecutionEndpoint'
+import { StorageHandler } from './notification-config/storageHandler'
+import { AmqpHandler } from './api/amqp/amqpHandler'
+import { TriggerEventHandler } from './api/triggerEventHandler'
 
 const port = 8080
-
-// authEnabled defaults to false
-const authEnabled: boolean = process.env.AUTH_ENABLED === 'true'
-if (!authEnabled) {
-  console.warn('WARNING: Authentication is disabled! Make sure this option turned off in production!\n')
-}
 
 const sandboxExecutor = new VM2SandboxExecutor()
 const notificationExecutor = new NotificationExecutor(sandboxExecutor)
@@ -36,17 +30,16 @@ const notificationConfigEndpoint = new NotificationConfigEndpoint(storageHandler
 const notificationExecutionEndpoint = new NotificationExecutionEndpoint(triggerEventHandler, app)
 
 app.listen(port, async () => {
-
   await amqpHandler.connect(30, 2000)
   await storageHandler.init(30, 2000)
 
   console.log('listening on port ' + port)
 
-  app.get("/", (req: express.Request, res: express.Response): void => {
+  app.get('/', (req: express.Request, res: express.Response): void => {
     res.send('I am alive!')
   })
 
-  app.get("/version", (req: express.Request, res: express.Response): void => {
+  app.get('/version', (req: express.Request, res: express.Response): void => {
     res.header('Content-Type', 'text/plain')
     res.send(notificationExecutor.getVersion())
     res.end()
