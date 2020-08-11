43ad834add84d19a95f23fd57c68f0ab12e9c0be
Georg Schwarz <georg.schwarz@fau.de>
2020-08-11 14:03:20 +0200

    Storage-mq: refactor postgres connection and query execution error handling and logging into own class

--- storage/storage-mq/src/storage-content/postgresStorageContentRepository.ts
+++ storage/storage-mq/src/storage-content/postgresStorageContentRepository.ts
@@ -1,29 +1,24 @@
-import { Pool, PoolConfig, PoolClient, QueryResult } from 'pg'
+import { PoolConfig, QueryResult } from 'pg'
 import { StorageContentRepository } from './storageContentRepository'
+import PostgresRepository from '@/util/postgresRepository'
 import { StorageContent } from './storageContent'
 import { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PW, POSTGRES_DB, POSTGRES_SCHEMA } from '../env'
 
 export class PostgresStorageContentRepository implements StorageContentRepository {
-  connectionPool?: Pool = undefined
+  postgresRepository: PostgresRepository
+
+  constructor (postgresRepository: PostgresRepository) {
+    this.postgresRepository = postgresRepository
+  }
 
   /**
      * Initializes the connection to the database.
      * @param retries:  Number of retries to connect to the database
      * @param backoff:  Time in seconds to backoff before next connection retry
      */
-  public async init (retries: number, backoff: number): Promise<void> {
+  public async init (retries: number, backoffMs: number): Promise<void> {
     console.debug('Initializing PostgresStorageStructureRepository')
-    await this.initConnectionPool(retries, backoff)
-  }
 
-  /**
-     * Initializes a database connection to the storage-db service.
-     * @param retries:  Number of retries to connect to the database
-     * @param backoff:  Time in seconds to backoff before next connection retry
-     *
-     * @returns reject promise on failure to connect
-     */
-  private async initConnectionPool (retries: number, backoffMs: number): Promise<void> {
     const poolConfig: PoolConfig = {
       host: POSTGRES_HOST,
       port: POSTGRES_PORT,
@@ -34,135 +29,53 @@ export class PostgresStorageContentRepository implements StorageContentRepositor
       idleTimeoutMillis: 30000,
       connectionTimeoutMillis: backoffMs
     }
-    console.debug(`Connecting to database with config:\n${JSON.stringify(poolConfig)}`)
 
-    // try to establish connection
-    for (let i = 1; i <= retries; i++) {
-      console.info(`Initiliazing database connection (${i}/${retries})`)
-      let client
-      try {
-        const connectionPool = new Pool(poolConfig)
-        client = await connectionPool.connect()
-        await client.query('SELECT 1')
-        this.connectionPool = connectionPool
-        console.info('Successfully established database connection')
-        break
-      } catch (error) {
-        await this.sleep(backoffMs)
-      } finally {
-        if (client) {
-          client.release()
-        }
-      }
-    }
-
-    if (!this.connectionPool) {
-      return Promise.reject(new Error('Connection to databse could not be established.'))
-    }
-
-    console.info('Sucessfully established connection to storage-db database.')
-    return Promise.resolve()
-  }
-
-  private sleep (ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    await this.postgresRepository.init(poolConfig, retries, backoffMs)
   }
 
   async existsTable (tableIdentifier: string): Promise<boolean> {
-    console.debug(`Checking if table "${tableIdentifier}" exists`)
-    if (!this.connectionPool) {
-      return Promise.reject(new Error('No connnection pool available'))
-    }
-
-    let client!: PoolClient
-    try {
-      client = await this.connectionPool.connect()
-      const resultSet = await client.query(`SELECT to_regclass('${POSTGRES_SCHEMA}.${tableIdentifier}')`)
-      const tableExists = !!resultSet.rows[0].to_regclass
-      console.debug(`Table ${tableIdentifier} exists: ${tableExists}`)
-      return Promise.resolve(tableExists)
-    } catch (error) {
-      console.error(`Error when checking if table ${tableIdentifier} exists:\n${error}`)
-      return Promise.reject(error)
-    } finally {
-      if (client) {
-        client.release()
-      }
-    }
+    const resultSet =
+      await this.postgresRepository.executeQuery(`SELECT to_regclass('${POSTGRES_SCHEMA}.${tableIdentifier}')`, [])
+    const tableExists = !!resultSet.rows[0].to_regclass
+    return Promise.resolve(tableExists)
   }
 
   async getAllContent (tableIdentifier: string): Promise<StorageContent[] | undefined> {
-    console.debug(`Fetching all content from database, table "${tableIdentifier}"`)
-    if (!this.connectionPool) {
-      return Promise.reject(new Error('No connnection pool available'))
-    }
-
     const tableExists = await this.existsTable(tableIdentifier)
     if (!tableExists) {
       console.debug(`Table "${tableIdentifier}" does not exist - returning no data`)
       return Promise.resolve(undefined)
     }
 
-    let client!: PoolClient
-    try {
-      client = await this.connectionPool.connect()
-      const resultSet = await client.query(`SELECT * FROM "${POSTGRES_SCHEMA}"."${tableIdentifier}"`)
-      const content = this.toContents(resultSet)
-      return Promise.resolve(content)
-    } catch (error) {
-      console.error(`Could not get content from table ${tableIdentifier}: ${error}`)
-      return Promise.reject(error)
-    } finally {
-      if (client) {
-        client.release()
-      }
-    }
+    const resultSet =
+      await this.postgresRepository.executeQuery(`SELECT * FROM "${POSTGRES_SCHEMA}"."${tableIdentifier}"`, [])
+    const content = this.toContents(resultSet)
+    return Promise.resolve(content)
   }
 
   async getContent (tableIdentifier: string, contentId: string): Promise<StorageContent | undefined> {
-    console.debug(`Fetching content from database, table "${tableIdentifier}", id "${contentId}"`)
-    if (!this.connectionPool) {
-      return Promise.reject(new Error('No connnection pool available'))
-    }
-
     const tableExists = await this.existsTable(tableIdentifier)
     if (!tableExists) {
       console.debug(`Table "${tableIdentifier}" does not exist - returning no data`)
       return Promise.resolve(undefined)
     }
 
-    let client!: PoolClient
-    try {
-      client = await this.connectionPool.connect()
-      const resultSet = await client.query(
-        `SELECT * FROM "${POSTGRES_SCHEMA}"."${tableIdentifier}" WHERE id = $1`, [contentId]
-      )
-      const content = this.toContents(resultSet)
-      if (!content || !content[0]) {
-        console.debug(`No content found for table "${tableIdentifier}", id ${contentId}`)
-        return undefined
-      }
-      console.debug(
-        `Fetched content for table "${tableIdentifier}", id ${contentId}: ` +
-        `{ pipelineId: ${content[0].pipelineId}, timestamp: ${content[0].timestamp}, data: <omitted in log>}`
-      )
-      return Promise.resolve(content[0])
-    } catch (error) {
-      console.error(`Could not get content from table ${tableIdentifier} with id ${contentId}: ${error}`)
-      return Promise.reject(error)
-    } finally {
-      if (client) {
-        client.release()
-      }
+    const resultSet = await this.postgresRepository.executeQuery(
+      `SELECT * FROM "${POSTGRES_SCHEMA}"."${tableIdentifier}" WHERE id = $1`, [contentId]
+    )
+    const content = this.toContents(resultSet)
+    if (!content || !content[0]) {
+      console.debug(`No content found for table "${tableIdentifier}", id ${contentId}`)
+      return undefined
     }
+    console.debug(
+      `Fetched content for table "${tableIdentifier}", id ${contentId}: ` +
+      `{ pipelineId: ${content[0].pipelineId}, timestamp: ${content[0].timestamp}, data: <omitted in log>}`
+    )
+    return Promise.resolve(content[0])
   }
 
   async saveContent (tableIdentifier: string, content: StorageContent): Promise<number> {
-    console.debug('Saving storage content data')
-    if (!this.connectionPool) {
-      return Promise.reject(new Error('No connnection pool available'))
-    }
-
     delete content.id // id not under control of client
 
     // Generate Query-String
@@ -172,21 +85,9 @@ export class PostgresStorageContentRepository implements StorageContentRepositor
       VALUES ($1, $2, $3) RETURNING *`
     const values = [data, parseInt(content.pipelineId), content.timestamp]
 
-    let client!: PoolClient // Client to execute the query
-    try {
-      client = await this.connectionPool.connect()
-      const { rows } = await client.query(insertStatement, values)
-      console.debug('Content successfully persisted.')
-      return Promise.resolve(rows[0].id as number)
-    } catch (err) {
-      const errMsg = `Could not save content data: ${err}`
-      console.error(errMsg)
-      return Promise.reject(err)
-    } finally {
-      if (client) {
-        client.release()
-      }
-    }
+    const { rows } = await this.postgresRepository.executeQuery(insertStatement, values)
+    console.debug('Content successfully persisted.')
+    return Promise.resolve(rows[0].id as number)
   }
 
   toContents (resultSet: QueryResult<object>): StorageContent[] {
