a23dad2adb52ea864ee99a3a7ad2672dc197a409
Georg Schwarz <georg.schwarz@fau.de>
2020-08-11 11:27:45 +0200

    Storage-mq: refactor event consumption with delegation to amqp connectio nclass

--- storage/storage-mq/src/api/amqp/PipelineExecutionConsumer.ts
+++ storage/storage-mq/src/api/amqp/PipelineExecutionConsumer.ts
@@ -1,63 +1,25 @@
 import * as AMQP from 'amqplib'
+import AmqpConsumer from './amqpConsumer'
 import PipelineExecutionEventHandler from '../pipelineExecutionEventHandler'
 
-const amqpUrl = process.env.AMQP_URL!
-const pipelineExecutionExchange = process.env.AMQP_PIPELINE_EXECUTION_EXCHANGE!
-const pipelineExecutionQueue = process.env.AMQP_PIPELINE_EXECUTION_QUEUE!
-const pipelineExecutionTopic = process.env.AMQP_PIPELINE_EXECUTION_TOPIC!
-const pipelineExecutionSuccessTopic = process.env.AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC!
+const AMQP_URL = process.env.AMQP_URL!
+const AMQP_EXECUTION_EXCHANGE = process.env.AMQP_PIPELINE_EXECUTION_EXCHANGE!
+const AMQP_EXECUTION_QUEUE = process.env.AMQP_PIPELINE_EXECUTION_QUEUE!
+const AMQP_EXECUTION_TOPIC = process.env.AMQP_PIPELINE_EXECUTION_TOPIC!
+const AMQP_EXECUTION_SUCCESS_TOPIC = process.env.AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC!
 
 export class PipelineExecutionConsumer {
-  pipelineExecutionEventHandler: PipelineExecutionEventHandler
+  private consumer: AmqpConsumer
+  private pipelineExecutionEventHandler: PipelineExecutionEventHandler
 
   constructor (pipelineExecutionEventHandler: PipelineExecutionEventHandler) {
     this.pipelineExecutionEventHandler = pipelineExecutionEventHandler
+    this.consumer = new AmqpConsumer()
   }
 
-  /**
-     * Connects to Amqp Service and initializes a channel
-     * @param retries   Number of retries to connect to the notification-config db
-     * @param backoff   Time to wait until the next retry
-     */
-  public async connect (retries: number, backoff: number): Promise<void> {
-    console.log('AMQP URL: ' + amqpUrl)
-    for (let i = 1; i <= retries; i++) {
-      try {
-        const connection = await AMQP.connect(amqpUrl)
-        await this.initChannel(connection)
-        return
-      } catch (error) {
-        console.info(`Error connecting to RabbitMQ: ${error}. Retrying in ${backoff} seconds`)
-        console.info(`Connecting to Amqp handler (${i}/${retries})`)
-        await this.sleep(backoff)
-        continue
-      }
-    }
-    return Promise.reject(new Error(`Could not establish connection to AMQP Broker (${amqpUrl})`))
-  }
-
-  private sleep (ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-  private async initChannel (connection: AMQP.Connection): Promise<void> {
-    console.log(
-      `Initializing queue "${pipelineExecutionQueue}" on exchange "${pipelineExecutionExchange}" ` +
-      `with topic "${pipelineExecutionTopic}"`
-    )
-    const channel = await connection.createChannel()
-
-    await channel.assertExchange(pipelineExecutionExchange, 'topic', {
-      durable: false
-    })
-
-    const q = await channel.assertQueue(pipelineExecutionQueue, {
-      exclusive: false
-    })
-    await channel.bindQueue(q.queue, pipelineExecutionExchange, pipelineExecutionTopic)
-
-    await channel.consume(q.queue, this.consumeEvent)
-    console.info('Successfully initialized AMQP queue')
+  async init (retries: number, msBackoff: number): Promise<void> {
+    await this.consumer.init(AMQP_URL, retries, msBackoff)
+    return this.consumer.consume(AMQP_EXECUTION_EXCHANGE, AMQP_EXECUTION_TOPIC, AMQP_EXECUTION_QUEUE, this.consumeEvent)
   }
 
   // use the f = () => {} syntax to access this
@@ -67,7 +29,7 @@ export class PipelineExecutionConsumer {
       return
     }
     console.debug("[ConsumingEvent] %s:'%s'", msg.fields.routingKey, msg.content.toString())
-    if (msg.fields.routingKey === pipelineExecutionSuccessTopic) {
+    if (msg.fields.routingKey === AMQP_EXECUTION_SUCCESS_TOPIC) {
       await this.pipelineExecutionEventHandler.handleSuccess(JSON.parse(msg.content.toString()))
     } else {
       console.debug('Received unsubscribed event on topic %s - doing nothing', msg.fields.routingKey)
---
+++ storage/storage-mq/src/api/amqp/amqpConsumer.ts
@@ -0,0 +1,71 @@
+import * as AMQP from 'amqplib'
+
+export default class AmqpConsumer {
+  private connection?: AMQP.Connection
+
+  public async init (amqpUrl: string, retries: number, msBackoff: number): Promise<void> {
+    for (let i = 0; i <= retries; i++) {
+      try {
+        this.connection = await this.connect(amqpUrl)
+        return
+      } catch (error) {
+        console.error(`Error initializing the AMQP Client (${i}/${retries}):
+        ${error}. Retrying in ${msBackoff}...`)
+        await this.sleep(msBackoff)
+      }
+    }
+    return Promise.reject(new Error(`Could not connect to AMQP broker at ${amqpUrl}`))
+  }
+
+  private async connect (amqpUrl: string): Promise<AMQP.Connection> {
+    try {
+      const connection = await AMQP.connect(amqpUrl)
+      console.log(`Connection to amqp host at ${amqpUrl} successful`)
+      return connection
+    } catch (error) {
+      console.error(`Error connecting to amqp host at ${amqpUrl}: ${error}`)
+      throw error
+    }
+  }
+
+  private initChannel = async (connection: AMQP.Connection, exchange: string): Promise<AMQP.Channel> => {
+    try {
+      const channel = await connection.createChannel()
+      await channel.assertExchange(exchange, 'topic', {
+        durable: false
+      })
+      console.log(`Exchange ${exchange} successfully initialized.`)
+      return channel
+    } catch (error) {
+      console.error(`Error creating exchange ${exchange}: ${error}`)
+      throw error
+    }
+  }
+
+  private sleep (ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  public async consume (
+    exchange: string,
+    topic: string,
+    queueName: string,
+    consumeEvent: (msg: AMQP.ConsumeMessage | null) => void
+  ): Promise<void> {
+    if (!this.connection) {
+      console.error('Consume not possible, AMQP client not initialized.')
+      return Promise.reject(new Error('Consume not possible, AMQP client not initialized.'))
+    } else {
+      try {
+        const channel = await this.initChannel(this.connection, exchange)
+        const q = await channel.assertQueue(queueName, {
+          exclusive: false
+        })
+        await channel.bindQueue(q.queue, exchange, topic)
+        await channel.consume(q.queue, consumeEvent)
+      } catch (error) {
+        console.error(`Error subscribing to exchange ${exchange} under key ${topic}: ${error}`)
+      }
+    }
+  }
+}
