4e3674af7e347bba640588580cc10562e5a0c89a
Georg Schwarz <schwarz@group.riehle.org>
2020-08-12 10:25:16 +0200

    Storage-mq: refactor retry for connection into separate methods

--- storage/storage-mq/src/util/postgresRepository.ts
+++ storage/storage-mq/src/util/postgresRepository.ts
@@ -10,34 +10,40 @@ export default class PostgresRepository {
    * @returns reject promise on failure to connect
    */
   public async init (poolConfig: PoolConfig, retries: number, backoffMs: number): Promise<void> {
+    this.connectionPool = await this.connectWithRetry(poolConfig, retries, backoffMs)
+    console.info('Sucessfully established connection to database.')
+  }
+
+  private async connectWithRetry (poolConfig: PoolConfig, retries: number, backoffMs: number): Promise<Pool> {
     console.debug(`Connecting to database with config:\n${JSON.stringify(poolConfig)}`)
 
-    // try to establish connection
+    let lastError: Error | undefined
     for (let i = 1; i <= retries; i++) {
-      console.info(`Initiliazing database connection (${i}/${retries})`)
-      let client
+      console.info(`Attempting to connect to database on URL ${poolConfig.host}:${poolConfig.port} (${i}/${retries})`)
       try {
-        const connectionPool = new Pool(poolConfig)
-        client = await connectionPool.connect()
-        await client.query('SELECT 1')
-        this.connectionPool = connectionPool
-        console.info('Successfully established database connection')
-        break
-      } catch (error) {
-        await this.sleep(backoffMs)
-      } finally {
-        if (client) {
-          client.release()
-        }
+        return await this.connect(poolConfig)
+      } catch (err) {
+        lastError = err
+      }
+      await this.sleep(backoffMs)
+    }
+
+    return Promise.reject(lastError)
+  }
+
+  private async connect (poolConfig: PoolConfig): Promise<Pool> {
+    let client
+    try {
+      const connectionPool = new Pool(poolConfig)
+      client = await connectionPool.connect()
+      await client.query('SELECT 1')
+      console.info('Successfully established database connection')
+      return Promise.resolve(connectionPool)
+    } finally {
+      if (client) {
+        client.release()
       }
     }
-
-    if (!this.connectionPool) {
-      return Promise.reject(new Error('Connection to database could not be established.'))
-    }
-
-    console.info('Sucessfully established connection to database.')
-    return Promise.resolve()
   }
 
   private sleep (ms: number): Promise<void> {
