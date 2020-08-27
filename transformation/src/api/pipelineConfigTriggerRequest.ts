8f80b8decb4aa84bb507033eecccf1a6650bd9c6
jsone-studios <jsone-studios@users.noreply.github.com>
2020-08-27 15:03:14 +0200

    Apply changes requested by review

--- transformation/src/api/pipelineConfigTriggerRequest.ts
+++ transformation/src/api/pipelineConfigTriggerRequest.ts
@@ -1,3 +1,5 @@
+import { isObject, isNumber, isString } from '../validators'
+
 export interface PipelineConfigTriggerRequest {
   datasourceId: number;
 
@@ -11,19 +13,19 @@ export class PipelineConfigTriggerRequestValidator {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   validate (requestBody: any): requestBody is PipelineConfigTriggerRequest {
     this.errors = []
-    if (typeof requestBody !== 'object') {
+    if (!isObject(requestBody)) {
       this.errors.push('\'PipelineConfigTriggerRequest\' must be an object')
       return false
     }
     if (!('datasourceId' in requestBody)) {
       this.errors.push('\'datasourceId\' property is missing')
-    } else if (typeof requestBody.datasourceId !== 'number') {
+    } else if (!isNumber(requestBody.datasourceId)) {
       this.errors.push('\'datasourceId\' must be a number')
     }
-    if ('data' in requestBody && typeof requestBody.data !== 'object') {
+    if ('data' in requestBody && !isObject(requestBody.data)) {
       this.errors.push('\'data\' must be an object or array')
     }
-    if ('dataLocation' in requestBody && typeof requestBody.dataLocation !== 'string') {
+    if ('dataLocation' in requestBody && !isString(requestBody.dataLocation)) {
       this.errors.push('\'dataLocation\' must be a string')
     }
     if (!('data' in requestBody) && !('dataLocation' in requestBody)) {
--- transformation/src/pipeline-config/model/pipelineConfig.ts
+++ transformation/src/pipeline-config/model/pipelineConfig.ts
@@ -1,3 +1,5 @@
+import { isObject, isString, isNumber } from '../../validators'
+
 export interface PipelineConfig {
   id: number;
   datasourceId: number;
@@ -17,6 +19,9 @@ export interface Metadata {
   creationTimestamp: Date;
 }
 
+/**
+ * PipelineConfig data transfer object that clients must use when creating or updating a PipelineConfig
+ */
 export interface PipelineConfigDTO {
   datasourceId: number;
   transformation: TransformationConfig;
@@ -36,56 +41,56 @@ export class PipelineConfigDTOValidator {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   validate (pipelineConfig: any): pipelineConfig is PipelineConfigDTO {
     this.errors = []
-    if (typeof pipelineConfig !== 'object') {
+    if (!isObject(pipelineConfig)) {
       this.errors.push('\'PipelineConfig\' must be an object')
       return false
     }
 
     if (!('datasourceId' in pipelineConfig)) {
       this.errors.push('\'datasourceId\' property is missing')
-    } else if (typeof pipelineConfig.datasourceId !== 'number') {
+    } else if (!isNumber(pipelineConfig.datasourceId)) {
       this.errors.push('\'datasourceId\' property must be a number')
     }
 
     if (!('transformation' in pipelineConfig)) {
       // Missing transformation is not an error, assume identity transformation
       pipelineConfig.transformation = { func: 'return data;' }
-    } else if (typeof pipelineConfig.transformation !== 'object') {
+    } else if (!isObject(pipelineConfig.transformation)) {
       this.errors.push('\'transformation\' property must be an object')
     } else {
       if (!('func' in pipelineConfig.transformation)) {
         this.errors.push('\'transformation.func\' property is missing')
-      } else if (typeof pipelineConfig.transformation.func !== 'string') {
+      } else if (!isString(pipelineConfig.transformation.func)) {
         this.errors.push('\'transformation.func\' property must be a string')
       }
     }
 
     if (!('metadata' in pipelineConfig)) {
       this.errors.push('\'metadata\' property is missing')
-    } else if (typeof pipelineConfig.metadata !== 'object') {
+    } else if (!isObject(pipelineConfig.metadata)) {
       this.errors.push('\'metadata\' property must be an object')
     } else {
       if (!('author' in pipelineConfig.metadata)) {
         this.errors.push('\'metadata.author\' property is missing')
-      } else if (typeof pipelineConfig.metadata.author !== 'string') {
+      } else if (!isString(pipelineConfig.metadata.author)) {
         this.errors.push('\'metadata.author\' property must be a string')
       }
 
       if (!('displayName' in pipelineConfig.metadata)) {
         this.errors.push('\'metadata.displayName\' property is missing')
-      } else if (typeof pipelineConfig.metadata.displayName !== 'string') {
+      } else if (!isString(pipelineConfig.metadata.displayName)) {
         this.errors.push('\'metadata.displayName\' property must be a string')
       }
 
       if (!('license' in pipelineConfig.metadata)) {
         this.errors.push('\'metadata.license\' property is missing')
-      } else if (typeof pipelineConfig.metadata.license !== 'string') {
+      } else if (!isString(pipelineConfig.metadata.license)) {
         this.errors.push('\'metadata.license\' property must be a string')
       }
 
       if (!('description' in pipelineConfig.metadata)) {
         this.errors.push('\'metadata.description\' property is missing')
-      } else if (typeof pipelineConfig.metadata.description !== 'string') {
+      } else if (!isString(pipelineConfig.metadata.description)) {
         this.errors.push('\'metadata.description\' property must be a string')
       }
     }
