30eddd560605f84283099e986045ceb79bc1174a
jsone-studios <jsone-studios@users.noreply.github.com>
2020-09-16 08:50:35 +0200

    Fix documentation of sleep function

--- scheduler/src/sleep.ts
+++ scheduler/src/sleep.ts
@@ -1,6 +1,6 @@
 /**
- * Returns a Promise that waits for a specific time period (in seconds) and resolves afterwards
- * @param ms Period to wait in seconds
+ * Returns a Promise that waits for a specific time period (in milliseconds) and resolves afterwards
+ * @param ms Period to wait in milliseconds
  */
 export function sleep (ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
