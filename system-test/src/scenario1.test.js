7c2cc74f08917a7f2d0e65cdadad415c1d3cd2f9
Andreas Bauer <andi.bauer@fau.de>
2020-09-25 22:29:19 +0200

    Refactor scenario1

--- system-test/src/scenario1.test.js
+++ system-test/src/scenario1.test.js
@@ -1,57 +1,25 @@
 const request = require('supertest')
-const waitOn = require('wait-on')
 const {
   STORAGE_URL,
-  SCHEDULER_URL,
   ADAPTER_URL,
   TRANSFORMATION_URL,
   NOTIFICATION_URL,
   MOCK_SERVER_URL,
-  MOCK_SERVER_WITHIN_DOCKER,
-  RABBIT_URL
+  MOCK_SERVER_WITHIN_DOCKER
 } = require('./env')
-
 const {
-  sleep,
   generateDataSourceConfig,
   generatePipelineConfig,
   generateSourceData, checkWebhook
 } = require('./testHelper')
+const { waitForServicesToBeReady } = require('./waitForServices')
 
 const TIMEOUT = 60000
-// This startup delay ensures that the scheduler will be reachable
-const STARTUP_DELAY = 2000
 
 let dataSourceConfig = null
 
-const notificationConfig = {
-  condition: 'data.one === 1',
-  url: MOCK_SERVER_WITHIN_DOCKER + '/notifications/test1',
-  pipelineId: -1
-}
-
 let dataSourceId = -1
 let pipelineId = -1
-let notificationId = -1
-
-async function waitForServicesToBeReady () {
-  const waitOptions = {
-    resources: [
-      STORAGE_URL,
-      SCHEDULER_URL,
-      TRANSFORMATION_URL,
-      NOTIFICATION_URL + '/',
-      ADAPTER_URL + '/version',
-      MOCK_SERVER_URL + '/',
-      RABBIT_URL
-    ],
-    timeout: TIMEOUT,
-    log: true
-  }
-
-  await waitOn(waitOptions)
-  await sleep(STARTUP_DELAY)
-}
 
 describe('Test 1: Create non-periodic pipeline without transformation', () => {
   beforeAll(async () => {
@@ -73,51 +41,54 @@ describe('Test 1: Create non-periodic pipeline without transformation', () => {
   }, TIMEOUT)
 
   test('Create DataSource at adapter service', async () => {
-    const adapterResponse = await request(ADAPTER_URL).post('/datasources').send(dataSourceConfig)
-    expect(adapterResponse.status).toEqual(201)
+    const response = await request(ADAPTER_URL).post('/datasources').send(dataSourceConfig)
+    expect(response.status).toEqual(201)
 
-    dataSourceId = adapterResponse.body.id
+    dataSourceId = response.body.id
     expect(dataSourceId).toBeGreaterThan(0)
   }, TIMEOUT)
 
   test('Add pipeline to DataSource', async () => {
     const pipelineConfig = generatePipelineConfig(dataSourceId)
-    const pipelineResponse = await request(TRANSFORMATION_URL).post('/configs').send(pipelineConfig)
-    expect(pipelineResponse.status).toEqual(201)
+    const response = await request(TRANSFORMATION_URL).post('/configs').send(pipelineConfig)
+    expect(response.status).toEqual(201)
 
-    pipelineId = pipelineResponse.body.id
+    pipelineId = response.body.id
     expect(pipelineId).toBeGreaterThan(0)
   }, TIMEOUT)
 
   test('Create notification', async () => {
-    notificationConfig.pipelineId = pipelineId
+    const notificationConfig = {
+      condition: 'data.one === 1',
+      url: MOCK_SERVER_WITHIN_DOCKER + '/notifications/test1',
+      pipelineId: pipelineId
+    }
 
-    const notificationResponse = await request(NOTIFICATION_URL).post('/config/webhook').send(notificationConfig)
-    expect(notificationResponse.status).toEqual(201)
-    notificationId = notificationResponse.body.id
-    expect(notificationId).toBeGreaterThan(0)
+    const response = await request(NOTIFICATION_URL).post('/config/webhook').send(notificationConfig)
+    expect(response.status).toEqual(201)
+    expect(response.body.id).toBeGreaterThan(0)
   }, TIMEOUT)
 
   test('Check notification web hook', async () => {
-    const webhookResponse = await checkWebhook(MOCK_SERVER_URL + '/notifications/test1')
-    expect(webhookResponse.body.location).toEqual(expect.stringContaining('/storage/' + pipelineId))
-    expect(webhookResponse.body.timestamp).toBeDefined()
+    const response = await checkWebhook(MOCK_SERVER_URL + '/notifications/test1')
+    expect(response.body.location).toEqual(expect.stringContaining('/storage/' + pipelineId))
+    expect(response.body.timestamp).toBeDefined()
   }, TIMEOUT)
 
   test('Check stored data', async () => {
-    const storageResponse = await request(STORAGE_URL).get('/' + pipelineId)
-    expect(storageResponse.status).toEqual(200)
-    expect(storageResponse.type).toEqual('application/json')
-    expect(storageResponse.body[0].data).toEqual(generateSourceData())
+    const response = await request(STORAGE_URL).get('/' + pipelineId)
+    expect(response.status).toEqual(200)
+    expect(response.type).toEqual('application/json')
+    expect(response.body[0].data).toEqual(generateSourceData())
   }, TIMEOUT)
 
   test('Delete transformation config', async () => {
-    const deletionResponse = await request(TRANSFORMATION_URL).delete(`/configs/${pipelineId}`).send()
-    expect(deletionResponse.status).toEqual(204)
+    const response = await request(TRANSFORMATION_URL).delete(`/configs/${pipelineId}`).send()
+    expect(response.status).toEqual(204)
   }, TIMEOUT)
 
   test('Delete adapter config', async () => {
-    const deletionResponse = await request(ADAPTER_URL).delete(`/datasources/${dataSourceId}`).send()
-    expect(deletionResponse.status).toEqual(204)
+    const response = await request(ADAPTER_URL).delete(`/datasources/${dataSourceId}`).send()
+    expect(response.status).toEqual(204)
   }, TIMEOUT)
 })
