c5d7a3b11a263020546625e578d7aa55d0186112
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-10-05 21:04:48 +0200

    Adapter: Expect amqp events in datasource it

--- adapter/integration-test/src/adapter-datasource.test.js
+++ adapter/integration-test/src/adapter-datasource.test.js
@@ -1,15 +1,34 @@
 const request = require('supertest')
 
 const {
-  ADAPTER_URL
+  ADAPTER_URL,
+  AMQP_URL
 } = require('./env')
 const { waitForServicesToBeReady } = require('./waitForServices')
+const {
+  connectAmqp,
+  consumeAmqpMsg
+} = require('./testHelper')
+
+const AMQP_EXCHANGE = 'ods_global'
+const AMQP_QUEUE = 'adapter-it-ds'
+const CONFIG_TOPIC = 'datasource.config.*'
+const CONFIG_CREATED_TOPIC = 'datasource.config.created'
+const CONFIG_DELETED_TOPIC = 'datasource.config.deleted'
+const CONFIG_UPDATED_TOPIC = 'datasource.config.updated'
 
 const TIMEOUT = 10000
+const publishedEvents = new Map() // routing key -> received msgs
+let amqpConnection
 
 describe('Datasource Configuration', () => {
   beforeAll(async () => {
     await waitForServicesToBeReady()
+
+    amqpConnection = await connectAmqp(AMQP_URL)
+
+    await consumeAmqpMsg(amqpConnection, AMQP_EXCHANGE, CONFIG_TOPIC, AMQP_QUEUE, publishedEvents)
+
   }, 60000)
 
   test('Should respond with stored datasource configs', async () => {
@@ -24,13 +43,18 @@ describe('Datasource Configuration', () => {
     const response = await request(ADAPTER_URL)
       .post('/datasources')
       .send(datasourceConfig)
+    const datasourceId = response.body.id
 
     expect(response.status).toEqual(201)
     expect(response.header.location).toContain(response.body.id)
     expect(response.body.adapter).toEqual(datasourceConfig.adapter)
     expect(response.body.trigger).toEqual(datasourceConfig.trigger)
     expect(response.body.id).toBeDefined()
-    expect(response.body.id).not.toEqual(datasourceConfig.id) // id not under control of client
+    expect(datasourceId).not.toEqual(datasourceConfig.id) // id not under control of client
+
+    expect(publishedEvents.get(CONFIG_CREATED_TOPIC)).toContainEqual({
+      datasourceId
+    })
 
     const delResponse = await request(ADAPTER_URL)
       .delete('/datasources/' + response.body.id)
@@ -57,6 +81,10 @@ describe('Datasource Configuration', () => {
       .send(updatedConfig)
 
     expect(putResponse.status).toEqual(204)
+
+    expect(publishedEvents.get(CONFIG_UPDATED_TOPIC)).toContainEqual({
+      datasourceId
+    })
 
     const updatedGetResponse = await request(ADAPTER_URL)
       .get('/datasources/' + datasourceId)
