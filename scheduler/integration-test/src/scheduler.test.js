90a40717f6c2a754d1067adaf5ef7061cfa5168a
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-10-07 14:46:01 +0200

    Scheduler-it: Remove deprecated tests

--- scheduler/integration-test/src/scheduler.test.js
+++ scheduler/integration-test/src/scheduler.test.js
@@ -48,25 +48,6 @@ describe('Scheduler-IT', () => {
     const semanticVersionRegEx = '^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)'
     expect(response.text).toMatch(new RegExp(semanticVersionRegEx))
   })
-
-  test('GET /jobs', async () => {
-    await sleep(4000) // wait until scheduler does sync
-    const response = await request(SCHEDULER_URL).get('/jobs')
-    expect(response.status).toEqual(200)
-    expect(response.type).toEqual('application/json')
-    expect(response.body).toHaveLength(2)
-    expect(response.body[0].scheduleJob).toBeDefined() // TODO: make explicit
-    expect(response.body[0].datasourceConfig.id).toEqual(1)
-    expect(response.body[1].datasourceConfig.id).toEqual(2)
-  })
-
-  test('Pipeline processes events', async () => {
-    await sleep(3000)
-    const response = await request(SCHEDULER_URL).get('/jobs')
-    expect(response.status).toEqual(200)
-    expect(response.type).toEqual('application/json')
-    expect(response.body).toHaveLength(2)
-  })
 })
 
 function sleep (ms) {
