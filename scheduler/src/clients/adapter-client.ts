3efbcfa46c24f7a81fdf27d531aebed3fddf934a
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-10-18 21:10:59 +0200

    Scheduler: Remove unused adapter client functions

--- scheduler/src/clients/adapter-client.ts
+++ scheduler/src/clients/adapter-client.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 import type DatasourceConfig from '../interfaces/datasource-config'
-import type { DatasourceEvent } from '../interfaces/datasource-event'
 
 import { ADAPTER_SERVICE_URL } from '../env'
 
@@ -13,21 +12,6 @@ const http = axios.create({
 export async function getAllDatasources (): Promise<DatasourceConfig[]> {
   const response = await http.get('/datasources')
   return response.data
-}
-
-export async function getDatasource (datasourceId: number): Promise<DatasourceConfig> {
-  const response = await http.get(`datasources/${datasourceId}`)
-  return response.data
-}
-
-export async function getEventsAfter (eventId: number): Promise<DatasourceEvent[]> {
-  const response = await http.get(`datasources/events?after=${eventId}`)
-  return response.data
-}
-
-export async function getLatestEventId (): Promise<number> {
-  const response = await http.get('datasources/events/latest')
-  return response.data.eventId ?? 0
 }
 
 export async function triggerDatasource (datasourceId: number): Promise<void> {
