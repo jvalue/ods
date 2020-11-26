ca6a52183053ebf9125e61d310b73ec82a9d8f7c
Georg Schwarz <dev@georg-schwarz.com>
2020-11-26 11:36:28 +0100

    Notification use @jvalue/node-dry-amqp (#253)
    * Notification: replace postgresRepository by library
    
    * Notification: use library for basic amqp functionality
    
    * Notification: rename amqpHandler to pipelineSuccessConsumer
    
    * Notification: extract amqp consumer config to explanatory variables

---
+++ notification/src/api/amqp/pipelineSuccessConsumer.ts
@@ -0,0 +1,61 @@
+import * as AMQP from 'amqplib'
+
+import { AmqpConsumer } from '@jvalue/node-dry-amqp'
+import { TriggerEventHandler } from '../triggerEventHandler'
+import {
+  AMQP_URL,
+  AMQP_PIPELINE_EXECUTION_EXCHANGE,
+  AMQP_PIPELINE_EXECUTION_QUEUE,
+  AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC
+} from '../../env'
+
+/**
+ * This class handles the communication with the AMQP service (rabbitmq)
+ * It interacts with these channels:
+ *
+ *      * Notification Channel:
+ *       ----------------------
+ *       A channel to notify the notification service that a transformation is done.
+ *       (see PipelineEvent for details of the event).
+ *
+ */
+export class PipelineSuccessConsumer {
+  amqpConsumer: AmqpConsumer = new AmqpConsumer()
+
+  constructor (private readonly triggerEventHandler: TriggerEventHandler) { }
+
+  /**
+     * Connects to Amqp Service and initializes a channel
+     *
+     * @param retries   Number of retries to connect to the notification-config db
+     * @param ms   Time to wait until the next retry
+     */
+  public async init (retries: number, ms: number): Promise<void> {
+    await this.amqpConsumer.init(AMQP_URL, retries, ms)
+
+    const exchange = { name: AMQP_PIPELINE_EXECUTION_EXCHANGE, type: 'topic' }
+    const exchangeOptions = {}
+    const queue = { name: AMQP_PIPELINE_EXECUTION_QUEUE, routingKey: AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC }
+    const queueOptions = { exclusive: false }
+    await this.amqpConsumer.registerConsumer(
+      exchange,
+      exchangeOptions,
+      queue,
+      queueOptions,
+      this.handleEvent
+    )
+  }
+
+  private readonly handleEvent = async (msg: AMQP.ConsumeMessage | null): Promise<void> => {
+    if (msg === null) {
+      console.debug('Received empty event when listening on pipeline executions - doing nothing')
+      return
+    }
+
+    if (msg.fields.routingKey === AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC) {
+      await this.triggerEventHandler.handleEvent(JSON.parse(msg.content.toString()))
+    } else {
+      console.debug('Received unsubscribed event on topic %s - doing nothing', msg.fields.routingKey)
+    }
+  }
+}
