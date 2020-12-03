7c81d9127e089b9ad9deebf272173afd7e675d1a
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-12-03 11:47:06 +0100

    Adapter-it: Remove unnecessary tests

--- adapter/integration-test/src/adapter-stateless.test.js
+++ adapter/integration-test/src/adapter-stateless.test.js
@@ -97,33 +97,6 @@ describe('Stateless data import', () => {
     expect(JSON.parse(response.body.data)).toEqual({ whateverwillbe: 'willbe', quesera: 'sera' })
   }, TIMEOUT)
 
-  test('Should create a RAW adapter as import [POST /dataImport]', async () => {
-    const reqBody = {
-      protocol: {
-        type: 'HTTP',
-        parameters: {
-          location: MOCK_SERVER_URL + '/xml',
-          encoding: 'UTF-8'
-        }
-      },
-      format: {
-        type: 'RAW'
-      }
-    }
-
-    const response = await request(ADAPTER_URL)
-      .post('/dataImport')
-      .query('includeData=true')
-      .send(reqBody)
-
-    expect(response.status).toEqual(200)
-    expect(response.body.id).toBeGreaterThan(0)
-    expect(response.body.data).toEqual(
-      '<?xml version="1.0" encoding="UTF-8"?>' +
-    '<root><from>Rick</from><to>Morty</to></root>'
-    )
-  })
-
   test('Should create a XML adapter as importer [POST /dataImport]', async () => {
     const reqBody = {
       protocol: {
