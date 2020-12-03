c02324bfc6082487d4b3f73c4c36dc3c60abab6d
Mathias Zinnen <mathias.zinnen@gmail.com>
2020-12-03 13:03:44 +0100

    Adapter: Fix exception handling for failed imports

--- adapter/src/main/java/org/jvalue/ods/adapterservice/adapter/api/rest/v1/AdapterEndpoint.java
+++ adapter/src/main/java/org/jvalue/ods/adapterservice/adapter/api/rest/v1/AdapterEndpoint.java
@@ -16,58 +16,58 @@ import javax.validation.Valid;
 @RequestMapping("/")
 public class AdapterEndpoint {
 
-  private final Adapter adapter;
+    private final Adapter adapter;
 
-  @Autowired
-  public AdapterEndpoint(Adapter adapter) {
-    this.adapter = adapter;
-  }
-
-
-  @PostMapping(value = Mappings.IMPORT_PATH, produces = "application/json")
-  public ResponseEntity<?> executeDataImport(
-    @Valid @RequestBody AdapterConfig config,
-    @RequestParam(required = false) boolean includeData) {
-    return this.executePreview(config, includeData);
-  }
-
-  @PostMapping(value = Mappings.PREVIEW_PATH, produces = "application/json")
-  public ResponseEntity<?> executePreview(
-          @Valid @RequestBody AdapterConfig config,
-          @RequestParam(required = false, defaultValue = "true") boolean includeData) {
-    try {
-      DataBlob imported = adapter.executeJob(config);
-
-      if(includeData) {
-        return ResponseEntity.ok(imported);
-      }
-
-      return ResponseEntity.ok(imported.getMetaData());
-
-    } catch (ResponseStatusException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    @Autowired
+    public AdapterEndpoint(Adapter adapter) {
+        this.adapter = adapter;
     }
-  }
 
-  @PostMapping(value = Mappings.RAW_PREVIEW_PATH, produces = "application/json")
-  public ResponseEntity<?> executeRawPreview(
-    @Valid @RequestBody ProtocolConfig config,
-    @RequestParam(required = false, defaultValue = "true") boolean includeData) {
-    try {
-      DataBlob imported = adapter.executeRawImport(config, includeData);
 
-      if(includeData) {
-        return ResponseEntity.ok(imported);
-      }
-
-      return ResponseEntity.ok(imported.getMetaData());
-
-    } catch (ResponseStatusException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    @PostMapping(value = Mappings.IMPORT_PATH, produces = "application/json")
+    public ResponseEntity<?> executeDataImport(
+            @Valid @RequestBody AdapterConfig config,
+            @RequestParam(required = false) boolean includeData) {
+        return this.executePreview(config, includeData);
     }
-  }
+
+    @PostMapping(value = Mappings.PREVIEW_PATH, produces = "application/json")
+    public ResponseEntity<?> executePreview(
+            @Valid @RequestBody AdapterConfig config,
+            @RequestParam(required = false, defaultValue = "true") boolean includeData) {
+        try {
+            DataBlob imported = adapter.executeJob(config);
+
+            if (includeData) {
+                return ResponseEntity.ok(imported);
+            }
+
+            return ResponseEntity.ok(imported.getMetaData());
+
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    @PostMapping(value = Mappings.RAW_PREVIEW_PATH, produces = "application/json")
+    public ResponseEntity<?> executeRawPreview(
+            @Valid @RequestBody ProtocolConfig config,
+            @RequestParam(required = false, defaultValue = "true") boolean includeData) {
+        try {
+            DataBlob imported = adapter.executeRawImport(config, includeData);
+
+            if (includeData) {
+                return ResponseEntity.ok(imported);
+            }
+
+            return ResponseEntity.ok(imported.getMetaData());
+
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
 }
