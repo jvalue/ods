7c58a9bb52a57e28f704007c211007fac22dc418
Karl Werner <info@zap.de.com>
2020-12-07 09:10:21 +0100

    UI: change test organization look everywhere for tests use jest directly instead of using vue-cli-service, because of watch mode do not fail with no tests

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -30,7 +30,7 @@ module.exports = {
   ],
 
   testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+    '**/*.(spec|test).(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
 
   testURL: 'http://localhost/',
