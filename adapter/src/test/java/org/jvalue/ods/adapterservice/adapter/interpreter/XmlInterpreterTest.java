02a7ceee35a16968561e3e7d04e4d6e6ec80a95d
Jonas <13821543+sonallux@users.noreply.github.com>
2021-01-18 17:27:16 +0100

    Adapter: update tools (#304)
    - Update to JDK 15
    - Update to gradle 6.7
    - Update to JUnit 5
    - Update adapter Dockerfile to use a docker image that already contains gradle

--- adapter/src/test/java/org/jvalue/ods/adapterservice/adapter/interpreter/XmlInterpreterTest.java
+++ adapter/src/test/java/org/jvalue/ods/adapterservice/adapter/interpreter/XmlInterpreterTest.java
@@ -1,106 +1,125 @@
 package org.jvalue.ods.adapterservice.adapter.interpreter;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class XmlInterpreterTest {
-    private final Interpreter interpreter = new XmlInterpreter();
-    private static final String XML_STRING = "<note><to>Walter Frosch</to><body>Nice game!</body></note>";
-    private final ObjectMapper mapper = new ObjectMapper();
-
-    @Test
-    public void interpretXmlData() throws IOException {
-        JsonNode result = interpreter.interpret(XML_STRING, Map.of());
-
-        assertEquals("{\"to\":\"Walter Frosch\",\"body\":\"Nice game!\"}", result.toString());
-    }
-
-    @Test
-    public void interpretXmlCollection() throws IOException {
-      final String collectionString =
-        "<menuItems>" +
-          "<pizza>" +
-            "<price>2</price>" +
-            "<taste>good</taste>" +
-          "</pizza>" +
-          "<pizza>" +
-            "<price>12</price>" +
-            "<taste>disgusting</taste>" +
-          "</pizza>" +
-        "</menuItems>";
-
-      JsonNode result = interpreter.interpret(collectionString, Map.of());
-
-      assertEquals(
-              "{\"pizza\":" +
-                        "[{\"price\":\"2\",\"taste\":\"good\"}," +
-                        "{\"price\":\"12\",\"taste\":\"disgusting\"}]}",
-              result.toString());
-    }
+  private final Interpreter interpreter = new XmlInterpreter();
+  private final ObjectMapper mapper = new ObjectMapper();
 
   @Test
-  public void interpretXmlCollectionNested() throws IOException {
-    final String collectionString =
-      "<menuItems>" +
-        "<menu>" +
-        "<pizza>" +
-        "<price>2</price>" +
-        "<taste>good</taste>" +
-        "</pizza>" +
-        "<pizza>" +
-        "<price>12</price>" +
-        "<taste>disgusting</taste>" +
-        "</pizza>" +
-        "</menu>" +
-        "</menuItems>";
+  public void interpretXmlData() throws IOException {
+    var xmlString = "<note><to>Walter Frosch</to><body>Nice game!</body></note>";
 
-    JsonNode result = interpreter.interpret(collectionString, Map.of());
+    var result = interpreter.interpret(xmlString, Map.of());
 
-    assertEquals(
-            "{\"menu\":" +
-                        "{\"pizza\":[" +
-                            "{\"price\":\"2\",\"taste\":\"good\"}," +
-                            "{\"price\":\"12\",\"taste\":\"disgusting\"}]}}",
-            result.toString()
+    assertThat(result.toString()).isEqualToIgnoringWhitespace("""
+      {"to": "Walter Frosch", "body": "Nice game!"}
+      """
     );
   }
 
-    @Test
-    public void interpretInconsistentXMLCollection() throws IOException {
-      final String collectionString =
-        "<menuItems>" +
-          "<pizza>" +
-            "<type>funghi</type>" +
-            "<taste>good</taste>" +
-          "</pizza>" +
-          "<pizza>" +
-            "<price>12</price>" +
-          "</pizza>" +
-        "</menuItems>";
+  @Test
+  public void interpretXmlCollection() throws IOException {
+    var collectionString = """
+      <menuItems>
+        <pizza>
+          <price>2</price>
+          <taste>good</taste>
+        </pizza>
+        <pizza>
+          <price>12</price>
+          <taste>disgusting</taste>
+        </pizza>
+      </menuItems>
+      """;
 
-      JsonNode result = interpreter.interpret(collectionString, Map.of());
+    var result = interpreter.interpret(collectionString, Map.of());
 
-      assertEquals(
-              "{\"pizza\":[{\"type\":\"funghi\",\"taste\":\"good\"},{\"price\":\"12\"}]}",
-              result.toString());
-    }
+    assertThat(result.toString()).isEqualToIgnoringWhitespace("""
+      {"pizza": [
+        {"price":"2","taste":"good"},
+        {"price":"12","taste":"disgusting"}
+      ]}
+      """
+    );
+  }
 
-    @Test(expected = IOException.class)
-    public void interpretMalformedData() throws IOException {
-        interpreter.interpret("{\"this is\":\"no xml\"", Map.of());
-    }
+  @Test
+  public void interpretXmlCollectionNested() throws IOException {
+    var collectionString = """
+      <menuItems>
+        <menu>
+          <pizza>
+            <price>2</price>
+            <taste>good</taste>
+          </pizza>
+          <pizza>
+            <price>12</price>
+            <taste>disgusting</taste>
+          </pizza>
+        </menu>
+      </menuItems>
+      """;
 
-    @Test
-    public void testSerialization() throws IOException {
-        JsonNode expected = mapper.readTree("{\"type\":\"XML\",\"description\":\"Interpret data as XML data\",\"parameters\":[]}");
-        JsonNode result = mapper.valueToTree(interpreter);
+    var result = interpreter.interpret(collectionString, Map.of());
 
-        assertEquals(expected, result);
-    }
+    assertThat(result.toString()).isEqualToIgnoringWhitespace("""
+      {"menu":
+        {"pizza":[
+          {"price":"2","taste":"good"},
+          {"price":"12","taste":"disgusting"}
+        ]}
+      }
+      """
+    );
+  }
+
+  @Test
+  public void interpretInconsistentXMLCollection() throws IOException {
+    var collectionString = """
+      <menuItems>
+        <pizza>
+          <type>funghi</type>
+          <taste>good</taste>
+        </pizza>
+        <pizza>
+          <price>12</price>
+        </pizza>
+      </menuItems>
+      """;
+
+    var result = interpreter.interpret(collectionString, Map.of());
+
+    assertThat(result.toString()).isEqualToIgnoringWhitespace("""
+      {"pizza": [
+        {"type": "funghi","taste": "good"},
+        {"price":"12"}
+      ]}
+      """
+    );
+  }
+
+  @Test
+  public void interpretMalformedData() {
+    assertThrows(IOException.class, () ->
+      interpreter.interpret("{\"this is\":\"no xml\"", Map.of())
+    );
+  }
+
+  @Test
+  public void testSerialization() throws IOException {
+    var expected = mapper.readTree("{\"type\":\"XML\",\"description\":\"Interpret data as XML data\",\"parameters\":[]}");
+    var result = mapper.valueToTree(interpreter);
+
+    assertEquals(expected, result);
+  }
 
 }
