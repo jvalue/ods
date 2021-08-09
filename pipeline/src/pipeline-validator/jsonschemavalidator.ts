da4b60633dc1e9a411b5323d8fa066c82ed241bd
Alexander Mahler <alex.domi.mahler@googlemail.com>
2021-08-10 00:16:11 +0200

    CI build bug

--- pipeline/src/pipeline-validator/jsonschemavalidator.ts
+++ pipeline/src/pipeline-validator/jsonschemavalidator.ts
@@ -1,6 +1,6 @@
 import Ajv from 'ajv'
-import { HealthStatus, PipelineConfig } from '../pipeline-config/model/pipelineConfig'
-import { PipelineTransformedDataDTO } from '../pipeline-config/model/pipelineTransformedData'
+import { HealthStatus, PipelineConfig } from './../pipeline-config/model/pipelineConfig'
+import { PipelineTransformedDataDTO } from './../pipeline-config/model/pipelineTransformedData'
 import Validator from './validator'
 
 export default class JsonSchemaValidator implements Validator {
