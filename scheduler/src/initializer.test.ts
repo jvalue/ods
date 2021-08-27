f177607ca95377fa295e9b7de1b957816c746e99
F3l1x <felix.mueller0998@gmx.de>
2021-08-27 11:16:16 +0200

    Applied new eslintrc to scheduler

--- scheduler/src/initializer.test.ts
+++ scheduler/src/initializer.test.ts
@@ -1,29 +1,29 @@
 /* eslint-env jest */
-import { mocked } from 'ts-jest/utils'
+import { mocked } from 'ts-jest/utils';
 
-import { getAllDatasources } from './api/http/adapter-client'
-import Scheduler from './scheduling'
-import { setupInitialStateWithRetry } from './initializer'
+import { getAllDatasources } from './api/http/adapter-client';
+import { setupInitialStateWithRetry } from './initializer';
+import Scheduler from './scheduling';
 
-jest.mock('./api/http/adapter-client')
-const mockedGetAllDatasources = mocked(getAllDatasources)
+jest.mock('./api/http/adapter-client');
+const mockedGetAllDatasources = mocked(getAllDatasources);
 
-jest.mock('./env', () => ({ MAX_TRIGGER_RETRIES: 2 }))
+jest.mock('./env', () => ({ MAX_TRIGGER_RETRIES: 2 }));
 
-const CONNECTION_RETRIES = 2
-const CONNECTION_BACKOFF_IN_MS = 1000
-const TRIGGER_RETRIES = 3
+const CONNECTION_RETRIES = 2;
+const CONNECTION_BACKOFF_IN_MS = 1000;
+const TRIGGER_RETRIES = 3;
 
-let scheduler: Scheduler
+let scheduler: Scheduler;
 
 describe('Scheduler initializer', () => {
   beforeEach(() => {
-    scheduler = new Scheduler(TRIGGER_RETRIES)
-  })
+    scheduler = new Scheduler(TRIGGER_RETRIES);
+  });
 
   afterEach(() => {
-    scheduler.removeAllJobs()
-  })
+    scheduler.removeAllJobs();
+  });
 
   test('should initialize jobs correctly', async () => {
     const config = {
@@ -31,13 +31,13 @@ describe('Scheduler initializer', () => {
       trigger: {
         periodic: false,
         firstExecution: new Date(),
-        interval: 60000
-      }
-    }
-    mockedGetAllDatasources.mockResolvedValue([config])
+        interval: 60000,
+      },
+    };
+    mockedGetAllDatasources.mockResolvedValue([config]);
 
-    await setupInitialStateWithRetry(scheduler, CONNECTION_RETRIES, CONNECTION_BACKOFF_IN_MS)
-    expect(scheduler.getAllJobs()).toHaveLength(1)
-    expect(scheduler.getAllJobs()[0].datasourceConfig).toEqual(config)
-  })
-})
+    await setupInitialStateWithRetry(scheduler, CONNECTION_RETRIES, CONNECTION_BACKOFF_IN_MS);
+    expect(scheduler.getAllJobs()).toHaveLength(1);
+    expect(scheduler.getAllJobs()[0].datasourceConfig).toEqual(config);
+  });
+});
--- scheduler/src/scheduling.ts
+++ scheduler/src/scheduling.ts
@@ -1,108 +1,108 @@
-import deepEqual from 'deep-equal'
-import schedule from 'node-schedule'
+import deepEqual from 'deep-equal';
+import schedule from 'node-schedule';
 
-import type DatasourceConfig from './api/datasource-config'
-import { triggerDatasource } from './trigger-handler'
+import type DatasourceConfig from './api/datasource-config';
+import { triggerDatasource } from './trigger-handler';
 
 export default class Scheduler {
-  private readonly allJobs: Map<number, SchedulingJob> = new Map() // datasourceId -> job
+  private readonly allJobs: Map<number, SchedulingJob> = new Map(); // DatasourceId -> job
 
-  constructor (private readonly triggerRetries: number) {
+  constructor(private readonly triggerRetries: number) {}
+
+  getJob(datasourceId: number): SchedulingJob | undefined {
+    return this.allJobs.get(datasourceId);
   }
 
-  getJob (datasourceId: number): SchedulingJob | undefined {
-    return this.allJobs.get(datasourceId)
+  getAllJobs(): SchedulingJob[] {
+    return Array.from(this.allJobs.values());
   }
 
-  getAllJobs (): SchedulingJob[] {
-    return Array.from(this.allJobs.values())
+  removeJob(datasourceId: number): void {
+    this.cancelJob(datasourceId);
+    this.allJobs.delete(datasourceId);
   }
 
-  removeJob (datasourceId: number): void {
-    this.cancelJob(datasourceId)
-    this.allJobs.delete(datasourceId)
+  removeAllJobs(): void {
+    this.allJobs.forEach((job) => {
+      schedule.cancelJob(job.scheduleJob);
+    });
+    this.allJobs.clear();
   }
 
-  removeAllJobs (): void {
-    this.allJobs.forEach(job => {
-      schedule.cancelJob(job.scheduleJob)
-    })
-    this.allJobs.clear()
+  private cancelJob(jobId: number): void {
+    const job = this.allJobs.get(jobId);
+    job?.scheduleJob.cancel();
   }
 
-  private cancelJob (jobId: number): void {
-    const job = this.allJobs.get(jobId)
-    job?.scheduleJob.cancel()
+  existsJob(datasourceId: number): boolean {
+    return this.allJobs.has(datasourceId);
   }
 
-  existsJob (datasourceId: number): boolean {
-    return this.allJobs.has(datasourceId)
+  existsEqualJob(datasourceConfig: DatasourceConfig): boolean {
+    const job = this.getJob(datasourceConfig.id);
+    return job !== undefined && deepEqual(job.datasourceConfig, datasourceConfig);
   }
 
-  existsEqualJob (datasourceConfig: DatasourceConfig): boolean {
-    const job = this.getJob(datasourceConfig.id)
-    return job !== undefined && deepEqual(job.datasourceConfig, datasourceConfig)
-  }
-
-  determineExecutionDate (datasourceConfig: DatasourceConfig): Date {
-    let executionDate = datasourceConfig.trigger.firstExecution.getTime()
-    const now = Date.now()
+  determineExecutionDate(datasourceConfig: DatasourceConfig): Date {
+    let executionDate = datasourceConfig.trigger.firstExecution.getTime();
+    const now = Date.now();
 
     if (executionDate > now) {
-      return datasourceConfig.trigger.firstExecution
+      return datasourceConfig.trigger.firstExecution;
     }
 
-    const offset = (now - executionDate) % datasourceConfig.trigger.interval
-    executionDate = now + datasourceConfig.trigger.interval - offset
-    return new Date(executionDate)
+    const offset = (now - executionDate) % datasourceConfig.trigger.interval;
+    executionDate = now + datasourceConfig.trigger.interval - offset;
+    return new Date(executionDate);
   }
 
-  private scheduleDatasource (datasourceConfig: DatasourceConfig): SchedulingJob {
-    const datasourceId = datasourceConfig.id
+  private scheduleDatasource(datasourceConfig: DatasourceConfig): SchedulingJob {
+    const datasourceId = datasourceConfig.id;
 
     // Cancel current job for given datasource
-    this.cancelJob(datasourceId)
+    this.cancelJob(datasourceId);
 
-    const executionDate: Date = this.determineExecutionDate(datasourceConfig)
+    const executionDate: Date = this.determineExecutionDate(datasourceConfig);
     console.log(`datasource ${datasourceId} with consecutive pipelines scheduled
-      for next execution at ${executionDate.toLocaleString()}.`)
+      for next execution at ${executionDate.toLocaleString()}.`);
 
-    const scheduledJob = schedule.scheduleJob(`Datasource ${datasourceId}`, executionDate,
-      () => this.execute(datasourceConfig))
-    const datasourceJob = { scheduleJob: scheduledJob, datasourceConfig: datasourceConfig }
-    this.allJobs.set(datasourceId, datasourceJob)
+    const scheduledJob = schedule.scheduleJob(`Datasource ${datasourceId}`, executionDate, () =>
+      this.execute(datasourceConfig),
+    );
+    const datasourceJob = { scheduleJob: scheduledJob, datasourceConfig: datasourceConfig };
+    this.allJobs.set(datasourceId, datasourceJob);
 
-    return datasourceJob
+    return datasourceJob;
   }
 
-  private execute (datasourceConfig: DatasourceConfig): void {
-    const datasourceId = datasourceConfig.id
+  private execute(datasourceConfig: DatasourceConfig): void {
+    const datasourceId = datasourceConfig.id;
     triggerDatasource(datasourceId, this.triggerRetries)
-      .catch(error => console.log('Failed to execute job:', error))
-      .finally(() => this.reschedule(datasourceConfig))
+      .catch((error) => console.log('Failed to execute job:', error))
+      .finally(() => this.reschedule(datasourceConfig));
   }
 
-  reschedule (datasourceConfig: DatasourceConfig): void {
+  reschedule(datasourceConfig: DatasourceConfig): void {
     if (datasourceConfig.trigger.periodic) {
-      this.scheduleDatasource(datasourceConfig)
+      this.scheduleDatasource(datasourceConfig);
     } else {
-      console.log(`Datasource ${datasourceConfig.id} is not periodic. Removing it from scheduling.`)
-      this.removeJob(datasourceConfig.id)
-      console.log(`Successfully removed datasource ${datasourceConfig.id} from scheduling.`)
+      console.log(`Datasource ${datasourceConfig.id} is not periodic. Removing it from scheduling.`);
+      this.removeJob(datasourceConfig.id);
+      console.log(`Successfully removed datasource ${datasourceConfig.id} from scheduling.`);
     }
   }
 
-  upsertJob (datasourceConfig: DatasourceConfig): SchedulingJob {
-    const isNewDatasource = !this.existsJob(datasourceConfig.id)
-    const datasourceState = isNewDatasource ? 'New' : 'Updated'
+  upsertJob(datasourceConfig: DatasourceConfig): SchedulingJob {
+    const isNewDatasource = !this.existsJob(datasourceConfig.id);
+    const datasourceState = isNewDatasource ? 'New' : 'Updated';
 
-    console.log(`[${datasourceState}] datasource detected with id ${datasourceConfig.id}.`)
+    console.log(`[${datasourceState}] datasource detected with id ${datasourceConfig.id}.`);
 
-    return this.scheduleDatasource(datasourceConfig)
+    return this.scheduleDatasource(datasourceConfig);
   }
 }
 
 interface SchedulingJob {
-  scheduleJob: schedule.Job
-  datasourceConfig: DatasourceConfig
+  scheduleJob: schedule.Job;
+  datasourceConfig: DatasourceConfig;
 }
