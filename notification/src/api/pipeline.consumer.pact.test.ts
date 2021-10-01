7382d7f154f3e74a4ae1c3c1d37ad82ce4d00fc2
Felix Quast <51856713+felix-oq@users.noreply.github.com>
2021-10-01 11:40:41 +0200

    Fix lint errors

--- notification/src/api/pipeline.consumer.pact.test.ts
+++ notification/src/api/pipeline.consumer.pact.test.ts
@@ -1,64 +1,73 @@
-import { asynchronousBodyHandler, Matchers } from '@pact-foundation/pact'
-import { JestMessageConsumerOptions, messagePactWith } from 'jest-pact'
-import path from 'path'
-import { PostgresNotificationRepository } from '../notification-config/postgresNotificationRepository'
-import NotificationExecutor from '../notification-execution/notificationExecutor'
-import { PipelineSuccessEvent } from './pipelineEvent'
-import { TriggerEventHandler } from './triggerEventHandler'
-import * as fs from 'fs'
+import * as fs from 'fs';
+import path from 'path';
 
-const pactsDir = path.resolve(process.cwd(), '..', 'pacts')
+import { Matchers, asynchronousBodyHandler } from '@pact-foundation/pact';
+import { JestMessageConsumerOptions, messagePactWith } from 'jest-pact';
+
+import { PostgresNotificationRepository } from '../notification-config/postgresNotificationRepository';
+import NotificationExecutor from '../notification-execution/notificationExecutor';
+
+import { PipelineSuccessEvent } from './pipelineEvent';
+import { TriggerEventHandler } from './triggerEventHandler';
+
+const pactsDir = path.resolve(process.cwd(), '..', 'pacts');
 
 const options: JestMessageConsumerOptions = {
   consumer: 'Notification',
   provider: 'Pipeline',
   dir: pactsDir,
-  logDir: path.resolve(process.cwd(), '..', 'pacts', 'logs')
+  logDir: path.resolve(process.cwd(), '..', 'pacts', 'logs'),
 
   // TODO the pactfileWriteMode cannot be set due to an open issue in pact-js: https://github.com/pact-foundation/pact-js/issues/414
-  // pactfileWriteMode: 'overwrite'
-}
+  // PactfileWriteMode: 'overwrite'
+};
 // TODO remove this workaround as soon as the pactfileWriteMode can be set to 'overwrite' in JestMessageConsumerOptions
-const contractPath = path.resolve(pactsDir, 'notification-pipeline.json')
-if (fs.existsSync(contractPath)) fs.unlinkSync(contractPath)
+const contractPath = path.resolve(pactsDir, 'notification-pipeline.json');
+if (fs.existsSync(contractPath)) {
+  fs.unlinkSync(contractPath);
+}
 
 const examplePipelineSuccessEvent: PipelineSuccessEvent = {
   pipelineId: 1,
   pipelineName: 'some pipeline name',
-  data: {}
-}
+  data: {},
+};
 
-jest.mock('../env', () => ({ }))
+jest.mock('../env', () => ({}));
 
 jest.mock('../notification-config/postgresNotificationRepository', () => {
   return {
     PostgresNotificationRepository: jest.fn().mockImplementation(() => {
       return {
-        getForPipeline: jest.fn().mockResolvedValue([])
-      }
-    })
-  }
-})
-const mockPostgresNotificationRepository = PostgresNotificationRepository as jest.Mock<PostgresNotificationRepository>
+        getForPipeline: jest.fn().mockResolvedValue([]),
+      };
+    }),
+  };
+});
+const mockPostgresNotificationRepository =
+  PostgresNotificationRepository as jest.Mock<PostgresNotificationRepository>;
 
-jest.mock('../notification-execution/notificationExecutor')
-const mockNotificationExecutor = NotificationExecutor as jest.Mock<NotificationExecutor>
+jest.mock('../notification-execution/notificationExecutor');
+const mockNotificationExecutor =
+  NotificationExecutor as jest.Mock<NotificationExecutor>;
 
 messagePactWith(options, (messagePact) => {
   describe('receiving an amqp message', () => {
     it('handles a success event', async () => {
       const triggerEventHandler = new TriggerEventHandler(
         mockPostgresNotificationRepository(),
-        mockNotificationExecutor()
-      )
+        mockNotificationExecutor(),
+      );
 
-      return await messagePact
+      await messagePact
         .given('any state')
         .expectsToReceive('a success event')
-        .withContent(
-          Matchers.like(examplePipelineSuccessEvent)
-        )
-        .verify(asynchronousBodyHandler(async (body) => await triggerEventHandler.handleEvent(body)))
-    })
-  })
-})
+        .withContent(Matchers.like(examplePipelineSuccessEvent))
+        .verify(
+          asynchronousBodyHandler(
+            async (body) => await triggerEventHandler.handleEvent(body),
+          ),
+        );
+    });
+  });
+});
--- pipeline/src/api/amqp/notification.provider.pact.test.ts
+++ pipeline/src/api/amqp/notification.provider.pact.test.ts
@@ -1,89 +1,112 @@
-import * as AMQP from 'amqplib'
-import path from 'path'
-import * as EventPublisher from '../../pipeline-config/outboxEventPublisher'
-import { MessageProviderPact } from '@pact-foundation/pact'
-import { init } from '../../pipeline-config/pipelineDatabase'
-import { AmqpConnection } from '@jvalue/node-dry-amqp'
-import { AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC, AMQP_URL, CONNECTION_BACKOFF, CONNECTION_RETRIES } from '../../env'
-import { readEnvOrDie } from '@jvalue/node-dry-basics'
-import { PostgresClient } from '@jvalue/node-dry-pg'
+import path from 'path';
 
-const AMQP_PIPELINE_EXECUTION_EXCHANGE = readEnvOrDie('AMQP_PIPELINE_EXECUTION_EXCHANGE')
-const AMQP_PIPELINE_EXECUTION_QUEUE = readEnvOrDie('AMQP_PIPELINE_EXECUTION_QUEUE')
+import { AmqpConnection } from '@jvalue/node-dry-amqp';
+import { readEnvOrDie } from '@jvalue/node-dry-basics';
+import { PostgresClient } from '@jvalue/node-dry-pg';
+import { MessageProviderPact } from '@pact-foundation/pact';
+import * as AMQP from 'amqplib';
+
+import {
+  AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC,
+  AMQP_URL,
+  CONNECTION_BACKOFF,
+  CONNECTION_RETRIES,
+} from '../../env';
+import * as EventPublisher from '../../pipeline-config/outboxEventPublisher';
+import { init } from '../../pipeline-config/pipelineDatabase';
+
+const AMQP_PIPELINE_EXECUTION_EXCHANGE = readEnvOrDie(
+  'AMQP_PIPELINE_EXECUTION_EXCHANGE',
+);
+const AMQP_PIPELINE_EXECUTION_QUEUE = readEnvOrDie(
+  'AMQP_PIPELINE_EXECUTION_QUEUE',
+);
 
 describe('Pact Provider Verification', () => {
   const pact = new MessageProviderPact({
     provider: 'Provider Service',
     pactUrls: [
-      path.resolve(process.cwd(), '..', 'pacts', 'notification-pipeline.json')
+      path.resolve(process.cwd(), '..', 'pacts', 'notification-pipeline.json'),
     ],
     logDir: path.resolve(process.cwd(), '..', 'pacts', 'logs'),
     stateHandlers: {
-      'any state': async () => {}
+      'any state': async (): Promise<void> => Promise.resolve(),
     },
     messageProviders: {
-      'a success event': provideSuccessEvent
-    }
-  })
+      'a success event': provideSuccessEvent,
+    },
+  });
 
-  let pgClient: PostgresClient
-  let amqpConnection: AmqpConnection
+  let pgClient: PostgresClient;
+  let amqpConnection: AmqpConnection;
 
-  const successMessages: unknown[] = []
+  const successMessages: unknown[] = [];
 
   beforeAll(async () => {
-    // setup amqp consumer to collect the messages that are published to the message-broker by the pipeline outboxer
-    pgClient = await init(CONNECTION_RETRIES, CONNECTION_BACKOFF)
+    // Setup amqp consumer to collect the messages that are published to the message-broker by the pipeline outboxer
+    pgClient = await init(CONNECTION_RETRIES, CONNECTION_BACKOFF);
 
-    amqpConnection = new AmqpConnection(AMQP_URL, CONNECTION_RETRIES, CONNECTION_BACKOFF,
+    amqpConnection = new AmqpConnection(
+      AMQP_URL,
+      CONNECTION_RETRIES,
+      CONNECTION_BACKOFF,
       () => {
-        console.error('lost connection to AMQP')
-        process.exit(1)
-      })
-    const amqpChannel = await amqpConnection.createChannel()
+        console.error('lost connection to AMQP');
+        process.exit(1);
+      },
+    );
+    const amqpChannel = await amqpConnection.createChannel();
 
-    await amqpChannel.assertExchange(AMQP_PIPELINE_EXECUTION_EXCHANGE, 'topic')
-    await amqpChannel.assertQueue(AMQP_PIPELINE_EXECUTION_QUEUE, { exclusive: false })
+    await amqpChannel.assertExchange(AMQP_PIPELINE_EXECUTION_EXCHANGE, 'topic');
+    await amqpChannel.assertQueue(AMQP_PIPELINE_EXECUTION_QUEUE, {
+      exclusive: false,
+    });
     await amqpChannel.bindQueue(
-      AMQP_PIPELINE_EXECUTION_QUEUE, AMQP_PIPELINE_EXECUTION_EXCHANGE, AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC)
+      AMQP_PIPELINE_EXECUTION_QUEUE,
+      AMQP_PIPELINE_EXECUTION_EXCHANGE,
+      AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC,
+    );
 
-    await amqpChannel.consume(AMQP_PIPELINE_EXECUTION_QUEUE, async (msg: AMQP.ConsumeMessage | null): Promise<void> => {
-      if (msg === null) {
-        console.error('received an AMQP message that was null')
-        process.exit(1)
-      }
-      if (msg.fields.routingKey === AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC) {
-        successMessages.push(JSON.parse(msg.content.toString()))
-      }
-      await amqpChannel.ack(msg)
-    })
-  })
+    await amqpChannel.consume(
+      AMQP_PIPELINE_EXECUTION_QUEUE,
+      async (msg: AMQP.ConsumeMessage | null): Promise<void> => {
+        if (msg == null) {
+          console.error('received an AMQP message that was null');
+          process.exit(1);
+        }
+        if (msg.fields.routingKey === AMQP_PIPELINE_EXECUTION_SUCCESS_TOPIC) {
+          successMessages.push(JSON.parse(msg.content.toString()));
+        }
+        await amqpChannel.ack(msg);
+      },
+    );
+  });
 
   it('validates the expectations of the notification service', async () => {
-    return await pact.verify()
-  })
+    await pact.verify();
+  });
 
   afterAll(async () => {
-    await pgClient.close()
-    await amqpConnection.close()
-  })
+    await pgClient.close();
+    await amqpConnection.close();
+  });
 
-  async function provideSuccessEvent (): Promise<unknown> {
-    await pgClient.transaction(async client => {
+  async function provideSuccessEvent(): Promise<unknown> {
+    await pgClient.transaction(async (client) => {
       await EventPublisher.publishSuccess(
         client,
         1,
         'some pipeline name',
         {},
-        { some: 'schema' }
-      )
-    })
+        { some: 'schema' },
+      );
+    });
 
-    // wait until the message that will be published by the pipeline outboxer is collected
+    // Wait until the message that will be published by the pipeline outboxer is collected
     while (successMessages.length === 0) {
-      await new Promise(resolve => setTimeout(resolve, 50))
+      await new Promise((resolve) => setTimeout(resolve, 50));
     }
 
-    return successMessages.pop()
+    return successMessages.pop();
   }
-})
+});
