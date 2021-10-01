b4a242ef39b7d69d1eb6d08d0f8287e2d17deb80
F3l1x <felix.mueller0998@gmx.de>
2021-10-01 10:10:31 +0200

    Switched to jvalue/eslint-config package

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,141 +1,17 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  env: {
-    node: true,
-  },
+  extends: '@jvalue/eslint-config-jvalue/vue',
   settings: {
     'import/resolver': {
       alias: {
         map: [
           ['@', './src'],
-          ['monaco-editor', './node_modules/monaco-editor/esm/vs/editor/editor.api.d.ts'],
+          [
+            'monaco-editor',
+            './node_modules/monaco-editor/esm/vs/editor/editor.api.d.ts',
+          ],
         ],
         extensions: ['.js', '.ts', '.vue'],
       },
     },
   },
-  reportUnusedDisableDirectives: true,
-  overrides: [
-    {
-      files: ['*.js'],
-      // For now, require .js files (like .eslintrc.js) only to be formatted using Prettier.
-      plugins: ['prettier'],
-      extends: ['plugin:prettier/recommended'],
-      rules: {
-        'prettier/prettier': 'warn',
-      },
-    },
-    {
-      files: ['*.ts', '*.vue'],
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-      extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-        '@vue/typescript/recommended',
-        '@vue/prettier',
-        '@vue/prettier/@typescript-eslint',
-      ],
-      rules: {
-        '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-        '@typescript-eslint/explicit-function-return-type': 'error',
-        '@typescript-eslint/explicit-member-accessibility': [
-          'error',
-          {
-            accessibility: 'no-public',
-          },
-        ],
-        '@typescript-eslint/member-ordering': [
-          'warn',
-          {
-            classes: ['field', 'constructor', 'method'],
-          },
-        ],
-        '@typescript-eslint/naming-convention': [
-          'error',
-          {
-            selector: ['class', 'interface', 'typeAlias'],
-            format: ['PascalCase'],
-          },
-        ],
-
-        // For instance, when defining a void event emitter method in a component, then we don't want to get a linter warning.
-        '@typescript-eslint/no-empty-function': 'off',
-
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-
-        'accessor-pairs': 'error',
-        'array-callback-return': 'error',
-        'capitalized-comments': ['warn', 'always'],
-        curly: 'error',
-        'default-case-last': 'error',
-        'default-param-last': 'error',
-        'dot-notation': 'error',
-        eqeqeq: ['error', 'always', { null: 'never' }],
-        'guard-for-in': 'error',
-        'no-constructor-return': 'error',
-        'no-else-return': 'error',
-        'no-extra-bind': 'error',
-        'no-lone-blocks': 'error',
-        'no-new-wrappers': 'error',
-        'no-nested-ternary': 'error',
-        'no-restricted-globals': [
-          'error',
-          { name: 'parseInt', message: `Use 'Number.parseInt()' instead.` },
-          { name: 'parseFloat', message: `Use 'Number.parseFloat()' instead.` },
-        ],
-        'no-self-compare': 'error',
-        'no-throw-literal': 'error',
-        'no-useless-rename': 'error',
-        'no-useless-return': 'error',
-        'import/no-cycle': 'error',
-        'import/order': [
-          'warn',
-          {
-            'newlines-between': 'always',
-            alphabetize: {
-              order: 'asc',
-              caseInsensitive: true,
-            },
-          },
-        ],
-        'no-unreachable-loop': 'error',
-        radix: 'error',
-        'require-atomic-updates': 'error',
-        'sort-imports': [
-          'warn',
-          {
-            ignoreDeclarationSort: true,
-          },
-        ],
-        'spaced-comment': ['warn', 'always'],
-        'valid-typeof': [
-          'error',
-          {
-            requireStringLiterals: true,
-          },
-        ],
-
-        'vue/html-self-closing': [
-          'warn',
-          {
-            html: {
-              void: 'always',
-              normal: 'never',
-              component: 'always',
-            },
-          },
-        ],
-      },
-    },
-  ],
 };
