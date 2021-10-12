cfe935a23706763fe3069395fdce1f1b748e40e5
Felix Quast <51856713+felix-oq@users.noreply.github.com>
2021-10-12 16:53:44 +0200

    Fix exit codes of CDCT-scripts

--- ui/cdct-consumer.sh
+++ ui/cdct-consumer.sh
@@ -5,4 +5,4 @@ set -e
 
 dir=$(dirname "$0")
 docker-compose -f "${dir}/../docker-compose.yml" -f "${dir}/../docker-compose.consumer.yml" build ui
-docker-compose -f "${dir}/../docker-compose.yml" -f "${dir}/../docker-compose.consumer.yml" up ui
+docker-compose -f "${dir}/../docker-compose.yml" -f "${dir}/../docker-compose.consumer.yml" up --exit-code-from ui ui
