7dcc9dffdcdb147fcd0c947ed224dc76745d7095
Felix Quast <51856713+felix-oq@users.noreply.github.com>
2021-10-13 11:42:52 +0200

    Use alternative option to don't start dependent services Uses --no-deps instead of possibly multiple --scale options

--- notification/cdct-consumer.sh
+++ notification/cdct-consumer.sh
@@ -5,4 +5,4 @@ set -e
 
 dir=$(dirname "$0")
 docker-compose -f "${dir}/../docker-compose.yml" -f "${dir}/../docker-compose.consumer.yml" build notification
-docker-compose -f "${dir}/../docker-compose.yml" -f "${dir}/../docker-compose.consumer.yml" up --exit-code-from notification --scale notification-db=0 --scale rabbitmq=0  notification
+docker-compose -f "${dir}/../docker-compose.yml" -f "${dir}/../docker-compose.consumer.yml" up --exit-code-from notification --no-deps notification
