a32aab1569ae28d734a07c180f9ad7f7cde6ac55
F3l1x <felix.mueller0998@gmx.de>
2022-01-13 10:39:08 +0100

    Deleted old Java adapter code/files

--- adapter/src/main/java/org/jvalue/ods/adapterservice/adapter/model/exceptions/ImporterParameterException.java
+++
@@ -1,7 +0,0 @@
-package org.jvalue.ods.adapterservice.adapter.model.exceptions;
-
-public class ImporterParameterException extends AdapterException {
-  public ImporterParameterException(String message) {
-    super(message);
-  }
-}
--- adapter/src/main/java/org/jvalue/ods/adapterservice/adapter/model/exceptions/InterpreterParameterException.java
+++
@@ -1,7 +0,0 @@
-package org.jvalue.ods.adapterservice.adapter.model.exceptions;
-
-public class InterpreterParameterException extends AdapterException {
-  public InterpreterParameterException(String message) {
-    super(message);
-  }
-}
