a08ecdc2cbe832a0110d882f678bad40689c9f51
F3l1x <felix.mueller0998@gmx.de>
2022-01-19 08:14:40 +0100

    Added dummy test, in order to allow building of adapter

---
+++ adapter/src/dummy.spec.ts
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+describe('dummy', () => {
+  test('dummy test', () => {
+    console.log(
+      'Dummy test - please remove after first real test implemented!',
+    );
+    expect(true).toBeTruthy();
+  });
+});
