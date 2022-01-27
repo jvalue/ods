eade23bf16a6caba545e78b8bc98a1856668e1cb
knusperkrone <aron.metzig@fau.de>
2022-01-27 20:07:21 +0100

    Entry point creates as RO user

--- storage/postgrest/entrypoint.sh
+++ storage/postgrest/entrypoint.sh
@@ -6,13 +6,18 @@ export PGRST_DB_SCHEMA=$DATABASE_SCHEMA
 export PGRST_DB_ANON_ROLE="${DATABASE_ANON}"
 
 psql --version
-printf 'Waiting for %s to be ready\n' "${DATABASE_URL}"
+printf 'Waiting for %s to be ready\n' "${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}"
 
 (( max_retry=30 ))
 (( current_retry=1 ))
 
+READ_ONLY_COMMAND="GRANT CONNECT ON DATABASE ${DATABASE_NAME} TO ${PGRST_DB_ANON_ROLE};
+GRANT USAGE ON SCHEMA ${PGRST_DB_SCHEMA} TO ${PGRST_DB_ANON_ROLE};
+GRANT SELECT ON ALL TABLES IN SCHEMA ${PGRST_DB_SCHEMA} TO ${PGRST_DB_ANON_ROLE};
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA ${PGRST_DB_SCHEMA} TO ${PGRST_DB_ANON_ROLE};"
+
 export PGPASSWORD=${DATABASE_PW}
-until psql -h "${DATABASE_HOST}" -p "${DATABASE_PORT}" -U "${DATABASE_USER}" -d "${DATABASE_NAME}" -c "select (1)" > /dev/null 2>&1 \
+until psql -h "${DATABASE_HOST}" -p "${DATABASE_PORT}" -U "${DATABASE_USER}" -d "${DATABASE_NAME}" -c "${READ_ONLY_COMMAND}" > /dev/null 2>&1 \
         || [ ${current_retry} -gt ${max_retry} ]; do
     printf "Attempt failed to connect to Postgres. (%s / %s)\n" "${current_retry}" "${max_retry}"
     current_retry=$(( current_retry + 1 ))
