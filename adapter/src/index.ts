249921d643c191ac357c196a75a52de36330362f
marco döll <marco.doell@hotmail.com>
2022-02-11 00:11:24 +0100

    added endpoints basic

--- adapter/src/index.ts
+++ adapter/src/index.ts
@@ -20,10 +20,90 @@ async function main(): Promise<void> {
     res.status(200).send('I am alive!');
   });
 
+  // Adapter Endpoint
+  /**
+     * @RestController
+  @AllArgsConstructor
+  public class AdapterEndpoint {
+    private final Adapter adapter;
+
+    @PostMapping(Mappings.IMPORT_PATH)
+    public DataImportResponse executeDataImport(@Valid @RequestBody AdapterConfig config)
+        throws ImporterParameterException, InterpreterParameterException, IOException {
+      return adapter.executeJob(config);
+    }
+
+    @PostMapping(Mappings.RAW_IMPORT_PATH)
+    public DataImportResponse executeRawPreview(@Valid @RequestBody ProtocolConfig config)
+        throws ImporterParameterException {
+      return adapter.executeRawImport(config);
+    }
+  }
+   */
+  app.post('/preview', (req: express.Request, res: express.Response): void => {
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send(API_VERSION);
+  });
+
+  app.post('/preview/raw', (req: express.Request, res: express.Response): void => {
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send(API_VERSION);
+  });
+
+  // Format Endpoint
+  /*
+    @RestController
+  @AllArgsConstructor
+  public class FormatEndpoint {
+    private final Adapter adapter;
+
+    @GetMapping(Mappings.FORMAT_PATH)
+    public Collection<Interpreter> getFormats() {
+      return adapter.getAllFormats();
+    }
+  } */
+  app.get('/formats', (req: express.Request, res: express.Response): void => {
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send(API_VERSION);
+  });
+
+  // Protocol Endpoint
+  /*
+    @RestController
+    @AllArgsConstructor
+  public class ProtocolEndpoint {
+    private final Adapter adapter;
+
+    @GetMapping(Mappings.PROTOCOL_PATH)
+    public Collection<Importer> getProtocols() {
+      return adapter.getAllProtocols();
+    }
+  }
+  */
+  app.get('/protocols', (req: express.Request, res: express.Response): void => {
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send(API_VERSION);
+  });
+
+  // Version Endpoint
+  /*
+  @RestController
+  public class VersionEndpoint {
+
+    @Value("${app.version}")
+    private String VERSION;
+
+    @GetMapping(Mappings.VERSION_PATH)
+    public String getApplicationVersion() {
+      return VERSION;
+    }
+  }
+  */
   app.get('/version', (req: express.Request, res: express.Response): void => {
     res.header('Content-Type', 'text/plain');
     res.status(200).send(API_VERSION);
   });
+
 
   server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
