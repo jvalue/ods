59bb3fea7a53994fb8c8e2af127ba2628afa5431
marco döll <marco.doell@hotmail.com>
2022-02-11 11:06:29 +0100

    lines

--- adapter/src/index.ts
+++ adapter/src/index.ts
@@ -20,10 +20,10 @@ async function main(): Promise<void> {
   app.get('/', (req: express.Request, res: express.Response): void => {
     res.status(200).send('I am alive!');
   });
-  const adapterEndpoint = new AdapterEndpoint();
 
+  const adapterEndpoint = new AdapterEndpoint();
   adapterEndpoint.registerRoutes(app);
- 
+
   server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
