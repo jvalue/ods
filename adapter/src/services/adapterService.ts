9af42c78e590a4c475c1ed42ab99a28402170778
Pascal Vahldiek <pascal.vahldiek@fau.de>
2022-02-11 11:30:07 +0100

    Test

--- adapter/src/services/adapterService.ts
+++ adapter/src/services/adapterService.ts
@@ -6,11 +6,11 @@ export class AdapterService {
      */
     constructor () {
     }
-  
+
     // To Implement
     static getAllFormats(): Array<Interpreter> {
       try {
-        let interpreters: Interpreter[] = [new Interpreter("TYPE_TEST", "Dies ist eine Beschreibung")]
+        let interpreters: Interpreter[] = [new Interpreter("TYPE_TEST", "Dies ist eine BeschreibungTestPV")]
         return interpreters
       } catch ( err ) {
         throw err
