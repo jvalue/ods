be7f1cbbc9677be1b508ee31bc6016e5f3a0957d
marco döll <marco.doell@hotmail.com>
2022-02-23 00:11:19 +0100

    adapterconfigvalidator with @PVahldiek

--- adapter/src/model/ProtocolConfig.ts
+++ adapter/src/model/ProtocolConfig.ts
@@ -1,20 +1,35 @@
-export class ProtocolConfig {
+import { Protocol } from "./enum/Protocol";
+
+export interface ProtocolConfig {
     
-    //protocol:Protocol;
+    protocol:Protocol;
     parameters: Map<string, any> | undefined;
-    //TODO JSON CREATER FROM JAVA
-    /*
-    JsonCreator
-  public ProtocolConfig(
-    @JsonProperty("type") Protocol protocol,
-    @JsonProperty("parameters") Map<String, Object> parameters) {
-    this.protocol = protocol;
-    this.parameters = parameters;
-  }
-    */
-    constructor(/*protocol: Protocol,*/ parameters: Map<string, any>) {
-      //this.protocol = protocol;
-      this.parameters = parameters;
+}
+
+export class AdapterConfigValidator {
+  private errors: string[] = [];
+
+  validate(request: unknown): request is AdapterConfig {
+    this.errors = [];
+    if (!validators.isObject(request)) {
+      this.errors.push("'AdapterConfig' must be an object");
+      return false;
     }
+    if (!validators.hasProperty(request, 'protocol')) {
+      this.errors.push("'protocol' property is missing");
+    } else if (!validators.isObject(request.protocol)) {
+      this.errors.push("'protocol' must be a string");
+    }
+    if (!validators.hasProperty(request, 'format')) {
+      this.errors.push("'format' property is missing");
+    } else if (!validators.isObject(request.format)) {
+      this.errors.push("'format' must be an object or array");
+    }
+    return this.errors.length === 0;
   }
+
+  getErrors(): string[] {
+    return this.errors;
+  }
+}
   
