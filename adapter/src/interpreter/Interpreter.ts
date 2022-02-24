2ddc14d6b10538b81eeb8bce2de220a69a5fb503
marco döll <marco.doell@hotmail.com>
2022-02-25 00:24:09 +0100

    interpreter with @PVahldiek

--- adapter/src/interpreter/Interpreter.ts
+++ adapter/src/interpreter/Interpreter.ts
@@ -3,43 +3,32 @@ export abstract class Interpreter {
   description: string | undefined;
   parameters: Record<string, unknown> | undefined;
 
-  /*
-    interpret(data: string, parameters: Map<string, any>): JsonNode { //throws IOException
-      validateParameters(parameters);
-      return doInterpret(data, parameters);
-      return null
-    }
-
-    abstract doInterpret(data: string, parameters: Map<string, any>): JsonNode //throws IOException;
-
-    validateParameters(inputParameters: Map<string, any>): JsonNode { //throws InterpreterParameterException
-        boolean illegalArguments = false;
-        String illegalArgumentsMessage = "";
-
-        for (InterpreterParameterDescription requiredParameter : getAvailableParameters()) {
-          if (inputParameters.get(requiredParameter.getName()) == null) {
-            illegalArguments = true;
-            illegalArgumentsMessage = illegalArgumentsMessage + getType() + " interpreter requires parameter "
-              + requiredParameter.getName() + "/n";
-
-          } else if (inputParameters.get(requiredParameter.getName()).getClass() != requiredParameter.getType()) {
-            illegalArguments = true;
-            illegalArgumentsMessage = illegalArgumentsMessage + getType() + " interpreter requires parameter "
-              + requiredParameter.getName() + " to be type " + requiredParameter.getType().toString() + "/n";
-          }
-        }
-        if (illegalArguments) {
-          throw new InterpreterParameterException(illegalArgumentsMessage);
-        }
-      }
-
+  
+  interpret(data: string, parameters: Record<string, unknown>): string { //throws IOException
+    this.validateParameters(parameters);
+    return this.doInterpret(data, parameters);
   }
 
-  /*
+  abstract doInterpret(data: string, parameters: Record<string, unknown>): string //throws IOException;
 
-    protected void validateParameters(Map<String, Object> inputParameters) throws InterpreterParameterException {
+  validateParameters(inputParameters: Record<string, unknown>) { //throws InterpreterParameterException
+      /*boolean illegalArguments = false;
+      String illegalArgumentsMessage = "";
 
+      for (InterpreterParameterDescription requiredParameter : getAvailableParameters()) {
+        if (inputParameters.get(requiredParameter.getName()) == null) {
+          illegalArguments = true;
+          illegalArgumentsMessage = illegalArgumentsMessage + getType() + " interpreter requires parameter "
+            + requiredParameter.getName() + "/n";
+
+        } else if (inputParameters.get(requiredParameter.getName()).getClass() != requiredParameter.getType()) {
+          illegalArguments = true;
+          illegalArgumentsMessage = illegalArgumentsMessage + getType() + " interpreter requires parameter "
+            + requiredParameter.getName() + " to be type " + requiredParameter.getType().toString() + "/n";
+        }
+      }
+      if (illegalArguments) {
+        throw new InterpreterParameterException(illegalArgumentsMessage);
+      }*/
     }
-
-  */
-}
+  }
