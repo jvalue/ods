398ce77ba2b3942f34ae6057f0eb51914cc7d95a
marco döll <marco.doell@hotmail.com>
2022-03-06 21:54:44 +0100

    finished csvinterpreter implementation with @PVahldiek

--- adapter/src/adapter/interpreter/CsvInterpreter.ts
+++ adapter/src/adapter/interpreter/CsvInterpreter.ts
@@ -1,5 +1,6 @@
 import {Interpreter} from "./Interpreter";
 import { InterpreterParameterDescription } from "./InterpreterParameterDescription";
+const csv=require('csvtojson')
 
 
 export class CsvInterpreter extends Interpreter {
@@ -20,63 +21,36 @@ export class CsvInterpreter extends Interpreter {
   override getAvailableParameters(): InterpreterParameterDescription[] {
     return this.parameters;
   }
-  /*
-    @Override
-  protected JsonNode doInterpret(String data, Map<String, Object> parameters) throws IOException {
-    CsvSchema csvSchema = createSchema(parameters);
-    if ((boolean) parameters.get("firstRowAsHeader")) {
-      return parseWithHeader(data, csvSchema);
-    } else {
-      return parseWithoutHeader(data, csvSchema);
-    }
-  }
-  */
+
   override doInterpret(data: string, parameters: Map<string, unknown>): string {
     let columnSeparator = (parameters.get("columnSeparator") as string).charAt(0)
     let lineSeparator: string = parameters.get("lineSeparator") as string;
+    let firstRowAsHeader: boolean = parameters.get("firstRowAsHeader") as boolean; // True = With header, False = WithoutHeader
     let skipFirstDataRow: boolean = parameters.get("skipFirstDataRow") as boolean;
-
-    var result = [];
-    var lines = data.split(lineSeparator);
-    var headers = lines[0].split(",");
-
-    for(var i=1; i<lines.length; i++) {
-
-      var obj = {};
-      var currentline = lines[i].split(lineSeparator);
-  
-      for(var j=0; j<headers.length; j++){
-        obj [headers[j]] = currentline[j];
-        obj["asdasd"] = currentline[j]
-      }
-  
-      result.push(obj);
-    }
     
+    let json: any[] = [];
+    let count = 0;
+    csv({
+      noheader: !firstRowAsHeader, // Be Careful: Need to Invert the boolean here
+      output: "json",
+      delimiter: columnSeparator,
+      eol: lineSeparator
+    })
+    .fromString(data)
+    .then((csvRow: any)=>{ 
+      if(skipFirstDataRow && ((count == 0 && !firstRowAsHeader) || (count == 1 && firstRowAsHeader))) {
+        // Skip First Row
+      }
+      else {      
+        json.push(csvRow);
+      }
 
-   
-
-    return JSON.stringify(result);
+      count = count+1;
+    })
+    
+    return JSON.stringify(json);
   }
 
-  /*
-    @Override
-  protected void validateParameters(Map<String, Object> inputParameters) throws InterpreterParameterException {
-    super.validateParameters(inputParameters);
-
-    String lineSeparator = (String) inputParameters.get("lineSeparator");
-    if (!lineSeparator.equals("\n") && !lineSeparator.equals("\r") && !lineSeparator.equals("\r\n")) {
-      throw new InterpreterParameterException(getType() + " interpreter requires parameter lineSeparator to have" +
-        " value \\n, \\r, or \\r\\n. Your given value " + lineSeparator + " is invalid!");
-    }
-
-    String columnSeparator = (String) inputParameters.get("columnSeparator");
-    if (columnSeparator.length() != 1) {
-      throw new InterpreterParameterException(getType() + " interpreter requires parameter columnSeparator to have" +
-        " length 1. Your given value " + columnSeparator + " is invalid!");
-    }
-  }
-  */
   override validateParameters(inputParameters: Map<string, unknown>): void {
       super.validateParameters(inputParameters);
       let lineSeparator: string = inputParameters.get("lineSeparator") as string;
@@ -92,47 +66,4 @@ export class CsvInterpreter extends Interpreter {
         " length 1. Your given value " + columnSeparator + " is invalid!");
     }
   }
-
-  /*  
-  private CsvSchema createSchema(Map<String, Object> parameters) {
-    CsvSchema csvSchema = CsvSchema
-      .emptySchema()
-      .withColumnSeparator(((String) parameters.get("columnSeparator")).charAt(0))
-      .withLineSeparator((String) parameters.get("lineSeparator"))
-      .withSkipFirstDataRow((boolean) parameters.get("skipFirstDataRow"));
-    if ((boolean) parameters.get("firstRowAsHeader")) {
-      csvSchema = csvSchema
-        .withHeader();
-    }
-    return csvSchema;
-  }
-
-  private JsonNode parseWithoutHeader(String data, CsvSchema csvSchema) throws IOException {
-    MappingIterator<Object[]> allLines = mapper
-      .readerFor(Object[].class)
-      .with(csvSchema)
-      .readValues(data);
-
-    ArrayNode result = mapper.createArrayNode();
-    while (allLines.hasNext()) {
-      result.add(jsonMapper.valueToTree(allLines.next()));
-    }
-
-    return result;
-  }
-
-  private JsonNode parseWithHeader(String data, CsvSchema csvSchema) throws IOException {
-    MappingIterator<Map<String, String>> allLines = mapper
-      .readerFor(Map.class)
-      .with(csvSchema)
-      .readValues(data);
-
-    ArrayNode result = mapper.createArrayNode();
-    while (allLines.hasNext()) {
-      result.add(jsonMapper.valueToTree(allLines.next()));
-    }
-
-    return result;
-  }
-   */
 }
