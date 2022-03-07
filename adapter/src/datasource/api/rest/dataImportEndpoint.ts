900e71c45da8e346061d6a161a370ee7ecbfe168
ChristoffSchaub <christoff.schaub@gmail.com>
2022-03-07 16:48:58 +0100

    add dataSourceManager skeleton

--- adapter/src/datasource/api/rest/dataImportEndpoint.ts
+++ adapter/src/datasource/api/rest/dataImportEndpoint.ts
@@ -1,45 +1,70 @@
 import express, {Express} from "express";
 import {asyncHandler} from "../../../adapter/api/rest/utils";
-
+const dataSourceManager = require( "../../DataSourceManager" );
 export class DataImportEndpoint {
 
   registerRoutes = (app: express.Application): void => {
-    app.get('/datasources/:datasourceId/imports', asyncHandler(this.getDataImportsForDatasource));
-    app.get('/datasources/:datasourceId/imports/latest', asyncHandler(this.getLatestDataImportForDatasource));
+    app.get('/datasources/:datasourceId/imports', asyncHandler(this.getMetaDataImportsForDatasource));
+    app.get('/datasources/:datasourceId/imports/latest', asyncHandler(this.getLatestMetaDataImportForDatasource));
     app.get('/datasources/:datasourceId/imports/latest/data', asyncHandler(this.getLatestDataImportForDatasource));
-    app.get('/datasources/:datasourceId/imports/:dataImportId', asyncHandler(this.getDataImportForDatasource));
-    app.get('/datasources/:datasourceId/imports/:dataImportId/data', asyncHandler(this.getDataImportForDatasource));
+    app.get('/datasources/:datasourceId/imports/:dataImportId', asyncHandler(this.getMetadataForDataImport));
+    app.get('/datasources/:datasourceId/imports/:dataImportId/data', asyncHandler(this.getDataFromDataImport));
   };
 
-  getDataImportsForDatasource = async (
+  getMetaDataImportsForDatasource = async (
     req: express.Request,
     res: express.Response,
   ): Promise<void> => {
 
     //  access param :
     //  res.send('profile with id' + req.params.id)
-    const param = req.params.datasourceId;
-    res.status(200).send("You called: /datasources/"+param+"/imports");
+    res.status(200).send( dataSourceManager.getMetaDataImportsForDatasource(req.params.datasourceId));
 
   };
+   getLatestMetaDataImportForDatasource = async (
+    req: express.Request,
+    res: express.Response,
+  ): Promise<void> => {
 
+  //  access param :
+  //  res.send('profile with id' + req.params.id)
+  res.status(200).send( dataSourceManager.getLatestMetaDataImportsForDatasource(req.params.datasourceId));
+
+};
   getLatestDataImportForDatasource = async (
     req: express.Request,
     res: express.Response,
   ): Promise<void> => {
     //  access param :
     //  res.send('profile with id' + req.params.id)
-    res.status(200).send("test");
+    res.status(200).send(dataSourceManager.getLatestDataImportForDatasource(req.params.datasourceId));
   };
 
-  getDataImportForDatasource = async (
+   getMetadataForDataImport = async (
+     req: express.Request,
+     res: express.Response,
+   ): Promise<void> => {
+     //  access param :
+     //  res.send('profile with id' + req.params.id)
+     const datasourceId = req.params.datasourceId;
+     const dataImportId= req.params.dataImportId;
+     res.status(200).send(dataSourceManager.getMetadataForDataImport(datasourceId,dataImportId));
+   };
+
+
+  getDataFromDataImport = async (
     req: express.Request,
     res: express.Response,
   ): Promise<void> => {
     //  access param :
     //  res.send('profile with id' + req.params.id)
-    const param = req.params.datasourceId;
+    const datasourceId = req.params.datasourceId;
     const dataImportId= req.params.dataImportId;
-    res.status(200).send("You called: /datasources/"+param+"/imports/"+dataImportId+"/data");
+    res.status(200).send(dataSourceManager.getDataFromDataImport(datasourceId,dataImportId));
+
   };
+
+
+
+
 }
