96c269c7ea2c0f80627175cff87a085df55893f5
= <=>
2022-04-21 14:02:18 +0200

    test push

--- adapter/src/adapter/interpreter/XmlInterpreter.ts
+++ adapter/src/adapter/interpreter/XmlInterpreter.ts
@@ -25,7 +25,7 @@ export class XmlInterpreter extends Interpreter {
     /* Data =
       '<?xml version="1.0" encoding="UTF-8"?>' +
       '<root><from>Rick</from><to>Morty</to>' +
-      '<rasdasd><s>Rick</s><t>Morty</t></rasdasd></root>';*/
+      '<rasdasd><s>Rick</s><t>Morty</t></rasdasd></root>';s*/
 
     const result = this.convertXmlToJson(data);
     const updatedResult: Record<string, unknown> = {};
