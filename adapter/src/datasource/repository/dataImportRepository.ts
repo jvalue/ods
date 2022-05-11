f22264bf85e8aef18eebe8b783a46ec13d10d641
MarcoDoell <marco.doell96@gmail.com>
2022-05-11 16:02:27 +0200

    default parameters with @Pvahldiek

--- adapter/src/datasource/repository/dataImportRepository.ts
+++ adapter/src/datasource/repository/dataImportRepository.ts
@@ -80,7 +80,7 @@ export class DataImportRepository {
 
   async getDataFromDataImport(datasourceId: string, dataImportId: string) {
     return await knex
-      .select('*')
+      .select()
       .from('public.data_import')
       .where('datasource_id', datasourceId)
       .andWhere('id', dataImportId);
@@ -98,15 +98,15 @@ export class DataImportRepository {
       data: KnexHelper.stringFromUTF8Array(dataImport[0].data),
     };
 
-    /* Const dataSource = await datasourceRepository.getDataSourceById(
+    const dataSource = await datasourceRepository.getDataSourceById(
       datasourceId,
     );
-    
 
     const keys = Object.keys(dataSource.protocol.parameters.defaultParameters);
     for (const entry of keys) {
       result[entry] = dataSource.protocol.parameters.defaultParameters[entry];
-    }*/
+    }
+
     if (JSON.parse(dataImport[0].parameters).parameters) {
       const parameters = JSON.parse(dataImport[0].parameters).parameters;
       const keys = Object.keys(parameters);
